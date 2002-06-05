8b67cb74665f08b7a11e4f73592580b84f0efe74
Grant Goodyear <g2boojum@gentoo.org>
2002-06-05 15:27:47 +0000

    Making it a tad prettier.

--- xml/old-web/python/genpkglist.py
+++ xml/old-web/python/genpkglist.py
@@ -4,8 +4,7 @@
 # Program generates a list of current packages and writes them to stdout.
 
 import genpkgxml
-pkglist = genpkgxml.getcurrentpkgs()
-for item in pkglist:
+for item in genpkgxml.getcurrentpkgs():
 	print "%s/%s-%s-%s" % (item[3],item[0],item[1],item[2])
 
 
