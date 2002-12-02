89efea17f56e39e0a51410d3b3ab7eaf5ca144c8
gweb <gweb>
2002-12-02 17:52:52 +0000

    added suppot to list all arches

--- admin/www.gentoo.org/scripts/nightly.sh
+++ admin/www.gentoo.org/scripts/nightly.sh
@@ -2,6 +2,8 @@
 #to get WEBROOT defined...
 echo ">>> Starting nightly Web update..."
 source ~/.bashrc
+#to get all arches in our package listing:
+export ACCEPT_KEYWORDS="x86 ppc alpha sparc sparc64"
 ${WEBROOT}/../scripts/cvs-page.sh
 ${WEBROOT}/../scripts/pkgs.py
 ${WEBROOT}/../scripts/use-index.sh
