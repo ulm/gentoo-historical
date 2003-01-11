a127223e276f1811d19672fc9db1de4d40e7fdc4
John P. Davis <zhen@gentoo.org>
2003-01-11 02:59:22 +0000

    fixed a lot of things, this now works correctly

--- admin/www.gentoo.org/scripts/doc-tarball.sh
+++ admin/www.gentoo.org/scripts/doc-tarball.sh
@@ -1,20 +1,12 @@
-#!/bin/bash																				
-#Creator: ZhEN																		
-#Date: 1/10/03																		
-#																							
-#This just creates the doc tarball that can be downloaded by users so that they can	
-#modify docs, submit new bugs, etc.													
-#																						
-#####################################################################################
+#!/bin/bash									
+[ -z ${WEBROOT} ] && echo "Webroot is not set, exiting" && exit 1
+TAR="/bin/tar"
 
-[ -z ${WEBROOT} ] && echo "Webroot is not set, exiting" &&	exit 1
-DOCROOT="${WEBROOT}/doc/"
-
-local x
+install -d ${WEBROOT}/dyn/doc-snapshots
 
 for x in cs de en es fr it ja kr nl
 do
-tar -cf ${WEBROOT}/dyn/docs-latest-${x}.tar ${DOCROOT}/${x} 2&1> /dev/null
-bzip2 -z ${WEBROOT}/dyn/docs-latest-${x}.tar 2&1> /dev/null
+cd ${WEBROOT}/doc
+${TAR} cjf ${WEBROOT}/dyn/doc-snapshots/docs-latest-${x}.tar.bz2 ${x}/ --exclude CVS 
 done
 
