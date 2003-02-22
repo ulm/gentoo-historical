d8d19c563a8081352380ddbdd704eaa996717ec1
Alain Penders <alain@gentoo.org>
2003-02-22 18:33:26 +0000

    Switched to LGPL, added readme, added installer, and one minor typo fix. Version 1.2

---
+++ users/alain/plugin/install/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "plugin",
+      version = "1.2",
+      description = "Python Plugin Framework",
+      py_modules = ["plugin"],
+      author = "Alain Penders <alain@rexorient.com>"
+      )
