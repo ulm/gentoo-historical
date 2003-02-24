f61312033d1f105d4d3ee2820628424565b18b39
Kurt Lieber <klieber@gentoo.org>
2003-02-24 02:00:38 +0000

    changed script to sync to new image location on ibiblio

--- admin/www.gentoo.org/scripts/image-upload.sh
+++ admin/www.gentoo.org/scripts/image-upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 [ -z "${WEBROOT}" ] && echo "\$WEBROOT not set; exiting" && exit 1
 echo 'Syncing up images to ibiblio...'
-rsync --delete -ae ssh ${WEBROOT}/../images/ drobbins@login.ibiblio.org:gentoo/images/
+rsync --delete -ae ssh ${WEBROOT}/../images/ drobbins@login.ibiblio.org:web-gentoo/images/
 echo "Images synchronized."
