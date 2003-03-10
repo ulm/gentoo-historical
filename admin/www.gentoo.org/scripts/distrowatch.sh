545eeb4c312bb1049f71a1ccac511e70ebdc2102
Kurt Lieber <klieber@gentoo.org>
2003-03-10 00:54:53 +0000

    added sudo to emerge sync so gweb can use it

--- admin/www.gentoo.org/scripts/distrowatch.sh
+++ admin/www.gentoo.org/scripts/distrowatch.sh
@@ -18,7 +18,8 @@
 SCRIPTDIR="/home/httpd/scripts/"
 TARGETDIR="/home/httpd/gentoo/xml/htdocs/dyn/"
 
-/usr/bin/emerge sync > /dev/null 2>&1
+# emerge sync needs to be run using sudo so gweb can do it.
+sudo /usr/bin/emerge sync > /dev/null 2>&1
 
 # gentoo_pkglist_x86.txt contains the stable branch packages
 ${SCRIPTDIR}pkglist.py 1> ${TARGETDIR}gentoo_pkglist_x86.txt 2> /dev/null
