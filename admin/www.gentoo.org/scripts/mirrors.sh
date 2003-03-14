cc6d185fd1b2b1c4eb74208d2d43791414598261
Rajiv Aaron Manglani <rajiv@gentoo.org>
2003-03-14 02:40:54 +0000

    source .bashrc to get vars defined.

--- admin/www.gentoo.org/scripts/mirrors.sh
+++ admin/www.gentoo.org/scripts/mirrors.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #
-# $Header: /var/cvsroot/gentoo/admin/www.gentoo.org/scripts/mirrors.sh,v 1.3 2003/03/14 01:25:24 rajiv Exp $
+# $Header: /var/cvsroot/gentoo/admin/www.gentoo.org/scripts/mirrors.sh,v 1.4 2003/03/14 02:40:54 rajiv Exp $
 #
 # this script creates lists of our (rsync) mirrors.
 # the list is used by mirrorselect.
 
+# to get WEBROOT and WEBSCRIPTS defined
+source ~/.bashrc
+
 [ -z "${WEBROOT}" ] && echo "\$WEBROOT not set; exiting" && exit 1
 
-echo ">>> Updatine rsync mirrors text list..."
+echo ">>> Updating rsync mirrors text list..."
 
 echo "# Gentoo rsync mirrors - <http://www.gentoo.org/>" > ${WEBROOT}/dyn/rsync-mirrors.txt
 echo -n "# Last updated: " >> ${WEBROOT}/dyn/rsync-mirrors.txt
