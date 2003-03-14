6b315a2072c1f30ff3ffead9617346b21b4fec8a
Rajiv Aaron Manglani <rajiv@gentoo.org>
2003-03-14 03:17:30 +0000

    sort output list.

--- admin/www.gentoo.org/scripts/mirrors.sh
+++ admin/www.gentoo.org/scripts/mirrors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# $Header: /var/cvsroot/gentoo/admin/www.gentoo.org/scripts/mirrors.sh,v 1.4 2003/03/14 02:40:54 rajiv Exp $
+# $Header: /var/cvsroot/gentoo/admin/www.gentoo.org/scripts/mirrors.sh,v 1.5 2003/03/14 03:17:30 rajiv Exp $
 #
 # this script creates lists of our (rsync) mirrors.
 # the list is used by mirrorselect.
@@ -16,6 +16,6 @@ echo "# Gentoo rsync mirrors - <http://www.gentoo.org/>" > ${WEBROOT}/dyn/rsync-
 echo -n "# Last updated: " >> ${WEBROOT}/dyn/rsync-mirrors.txt
 date >> ${WEBROOT}/dyn/rsync-mirrors.txt
 echo "#" >> ${WEBROOT}/dyn/rsync-mirrors.txt
-host rsync.gentoo.org | awk -F" " '{print $4}' >> ${WEBROOT}/dyn/rsync-mirrors.txt
+host rsync.gentoo.org | awk -F" " '{print $4}' | sort >> ${WEBROOT}/dyn/rsync-mirrors.txt
 
 echo ">>> Done."
