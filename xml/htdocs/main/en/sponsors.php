411db2c07c3ccf5f8bf5865131e5e7082c5873a6
Kurt Lieber <klieber@gentoo.org>
2003-03-16 17:14:52 +0000

    draft version of sponsors file

---
+++ xml/htdocs/main/en/sponsors.php
@@ -0,0 +1,94 @@
+<?xml version='1.0' encoding="UTF-8"?>
+<?xml-stylesheet href="/xsl/guide.xsl" type="text/xsl"?>
+<mainpage id="sponsors">
+<title>Gentoo Linux Sponsors</title>
+<author title="Author"><mail link="klieber@gentoo.org">Kurt Lieber</mail></author>
+
+<version>1.0</version>
+<date>16 March 2003</date>
+
+<chapter>
+<section>
+<title>Gentoo Linux... in a poster</title>
+<body>
+		<fig link="http://www.ibiblio.org/web-gentoo/images/poster.jpg"/>
+		<br/>
+</body>
+</section>
+<section>
+<title>Sponsors of Gentoo Linux</title>
+<body>
+		<p>
+		Gentoo Linux would like to thank the following companies and organizations who donate various services and equipment to further the development of Gentoo Linux.  Without these organizations, Gentoo Linux would not be where it is today.
+		</p>
+</body>
+</section>
+<section>
+<title>Oregon State University</title>
+<body>
+	<uri link="http://www.oregonstate.edu"><fig link="/images/sponsors/osu-logo.gif"/></uri>
+	<p>
+	Outstanding programs, people, and location make Oregon State an exciting and vibrant university--one that is internationally renowned. <uri link="http://www.oregonstate.edu">Oregon State University</uri> is Oregon's land grant, sea grant, and space grant university, with nearly 19,000 students from all 50 states and more than 80 countries.</p>
+	<p>
+	OSU provides several services to the Gentoo Linux project.  In addition to serving as the primary source mirror for Gentoo Linux, they also provide colocation space for several Gentoo Linux servers.
+	</p>
+</body>
+</section>
+<section>
+<title>Pilosoft</title>
+<body>
+	<fig link="/images/sponsors/pilosoft-logo.gif"/>
+	<p>
+	Pilosoft provides a multitude of Internet-related services, including DSL connectivity, shared and dedicated web hosting, colocation facilities and customized IP services.
+	</p>
+	<p>
+	Pilosoft provides bandwidth and colocation services for the Gentoo Linux project.
+	</p>
+</body>
+</section>
+<section>
+<title>Southern Nazarene University</title>
+<body>
+	<!--<fig link="/images/sponsors/pilosoft-logo.gif"/>-->
+	<p>
+	Founded in 1899, Southern Nazarene University is a private, Christian, liberal arts university-a service of the Church of the Nazarene. Located on a 40-acre campus just west of Oklahoma City, SNU grew out of several small colleges committed to training people for service to God and their fellow man. More than 25,000 alumni work and serve throughout the United States and the world.
+	</p>
+	<p>
+	SNU provides use of a dedicated server and bandwidth to the Gentoo Linux project.
+	</p>
+</body>
+</section>
+<section>
+<title>Source and rsync mirrors</title>
+<body>
+	<p>
+	Gentoo Linux relies heavily upon a global network of rsync and source mirrors.  Without these mirrors, access to new packages, updates to the Portage tree and new releases of Gentoo Linux would be impossible.  Some mirrors are provided by individuals, others by companies.  Below is a sampling of some of our mirror providers:</p>
+	<table>
+		<tr>
+			<th>Name</th><th>Mirror Type</th>
+		</tr>
+		<tr>
+			<td><uri link="http://www.ibiblio.org">ibiblio</uri></td><td>Source</td>
+		</tr>
+		<tr>
+			<td><uri link="http://www.ufl.edu">University of Florida</uri></td><td>rsync</td>
+		</tr>
+		<tr>
+			<td><uri link="http://www.netoptex.com">GNI</uri></td><td>rsync</td>
+		</tr>
+	</table>
+	<p>
+	<note>Since some sponsors wish to remain anonymous, we only list mirrors here upon request.  If you would like to see your mirror added to this list, please contact <mail link="klieber@gentoo.org">Kurt Lieber</mail></note>
+	</p>
+</body>
+</section>
+<section>
+<title>Other Sponsors</title>
+<body>
+	<p>
+	Several other companies and organizations provide services and assistance to the Gentoo Linux project, but have expressed a desire to remain anonymous.  We would like to thank those sponsors for their support of Gentoo Linux.
+	</p>
+</body>
+</section>
+</chapter>
+</mainpage>
