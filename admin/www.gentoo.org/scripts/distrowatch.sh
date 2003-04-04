c3df18b9aeb5f8176ea7ea5f871b32f33565ff5e
Daniel Robbins <drobbins@gentoo.org>
2003-04-04 19:55:19 +0000

    add support for non-x86 packages

--- admin/www.gentoo.org/scripts/distrowatch.sh
+++ admin/www.gentoo.org/scripts/distrowatch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# $Header: /var/cvsroot/gentoo/admin/www.gentoo.org/scripts/distrowatch.sh,v 1.4 2003/03/14 05:03:14 rajiv Exp $
+# $Header: /var/cvsroot/gentoo/admin/www.gentoo.org/scripts/distrowatch.sh,v 1.5 2003/04/04 19:55:19 drobbins Exp $
 #
 # this file creates a list of packages from the Portage tree for distrowatch
 #
@@ -10,12 +10,15 @@
 
 echo ">>> Updating distrowatch lists..."
 
+#Note: distrowatch lists non-x86 packages like yaboot, so I'm now including all arches (drobbins, 04/04/2003)
+STABLE_ARCHES="x86 ppc sparc mips hppa alpha"
+UNSTABLE_ARCHES="~x86 ~ppc ~sparc ~mips ~hppa ~alpha"
 # gentoo_pkglist_x86.txt contains the stable branch packages
 echo ">>> Generating stable package list..."
-ACCEPT_KEYWORDS="x86" ${WEBSCRIPTS}/pkglist.py 1> ${WEBROOT}/dyn/gentoo_pkglist_x86.txt 2> /dev/null
+ACCEPT_KEYWORDS="$STABLE_ARCHES" ${WEBSCRIPTS}/pkglist.py 1> ${WEBROOT}/dyn/gentoo_pkglist_x86.txt 2> /dev/null
 
 # gentoo_pkglist_X86.txt contains the unstable branch packages (note capital X)
 echo ">>> Generating unstable package list..."
-ACCEPT_KEYWORDS="~x86" ${WEBSCRIPTS}/pkglist.py 1> ${WEBROOT}/dyn/gentoo_pkglist_X86.txt 2> /dev/null
+ACCEPT_KEYWORDS="$STABLE_ARCHES $UNSTABLE_ARCHES" ${WEBSCRIPTS}/pkglist.py 1> ${WEBROOT}/dyn/gentoo_pkglist_X86.txt 2> /dev/null
 
 echo ">>> Done."
