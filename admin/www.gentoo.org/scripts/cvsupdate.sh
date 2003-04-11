2408cecedaf112ba733220df1630cbbf0543c844
Kurt Lieber <klieber@gentoo.org>
2003-04-11 14:46:25 +0000

    initial version of cvsupdate.sh file

---
+++ admin/www.gentoo.org/scripts/cvsupdate.sh
@@ -0,0 +1,18 @@
+﻿#!/bin/sh
+#
+# This script runs periodic cvs updates of the www.gentoo.org web site.
+#
+# AUTHOR: Kurt Lieber <klieber@gentoo.org>
+# VERSION: 0.1
+# DATE: April 11, 2003
+#
+######################################################################
+
+source ~/.bashrc
+
+# we actually want to go to gentoo/xml instead of gentoo/xml/htdocs
+# this ensures we update the gentoo/xml/images directory, too
+cd ${WEBROOT}/../
+cvs update -dPQ
+
+#done
