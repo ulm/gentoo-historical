9d4c0a22abfde0f4522b73c40c22f4b27f1145c2
John P. Davis <zhen@gentoo.org>
2003-10-02 18:38:32 +0000

    this script uploads snapshots of catalyst and my hardened profile

--- users/zhen/scripts/catshoot.sh
+++ users/zhen/scripts/catshoot.sh
@@ -2,4 +2,16 @@
 
 # <zhen@gentoo.org> a tool to upload my snapshots of catalyst and our default profile
 
+BASEDIR="/home/gentoo-cvs/gentoo/users/zhen"
+NICEDATE="`date +%Y%m%d`"
+PROFILE="hardened-x86-1.4"
 
+cd ${BASEDIR}/profiles
+tar cjf /tmp/${PROFILE}-${NICEDATE}.tar.bz2 ${PROFILE}
+
+cd ${BASEDIR}
+tar cjf /tmp/catalyst-${NICEDATE}.tar.bz2 catalyst
+
+scp /tmp/${PROFILE}-${NICEDATE}.tar.bz2 /tmp/catalyst-${NICEDATE}.tar.bz2 zhen@dev.gentoo.org:public_html/Hardened/snaps
+
+rm /tmp/${PROFILE}-${NICEDATE}.tar.bz2 /tmp/catalyst-${NICEDATE}.tar.bz2
