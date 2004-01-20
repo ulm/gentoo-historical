82b21384be76878ebb430f33fbda1f21c400b0ec
Rob Holland <tigger@gentoo.org>
2004-01-20 22:38:58 +0000

    tidied a little and changed to take its settings from gweb's bashrc

--- admin/www.gentoo.org/scripts/cvs-repo-update.sh
+++ admin/www.gentoo.org/scripts/cvs-repo-update.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 #
-# This script grabs a recent copy of the cvs repository
+# This script grabs a recent copy of various cvs repositories
+# so that viewcvs can do web views of them
 #
-# AUTHOR: Rob Holland <robh@gentoo.org>
-# VERSION: 0.1
-# DATE: July 7, 2003
+# AUTHOR: Rob Holland <tigger@gentoo.org>
+# VERSION: 1.0
+# DATE: 20th Janurary 2004
 #
 ######################################################################
 
 source ~/.bashrc
 
-WORKDIR=/home/httpd
 RSYNC_ARGS="--rsh=ssh -a --delete --delete-excluded"
 
-# create the directory if this is the first time this has run
-mkdir -p ${WORKDIR}/cvsroot
+if [ -z "${WEBCVSROOT}" -a -d "${WEBCVSROOT}" ]; then
+	# grab a recent copy of the cvs modules for viewcvs
 
-# grab a recent copy of the cvs modules for viewcvs
+	rsync ${RSYNC_ARGS} cvs.gentoo.org:/home/cvsroot/gentoo/ \
+	--exclude /home/cvsroot/gentoo/admin/ ${WEBCVSROOT}/gentoo/
 
-rsync ${RSYNC_ARGS} cvs.gentoo.org:/home/cvsroot/gentoo/ \
---exclude /home/cvsroot/gentoo/admin/ ${WORKDIR}/cvsroot/gentoo/
+	rsync ${RSYNC_ARGS} cvs.gentoo.org:/home/cvsroot/gentoo-x86/ \
+	${WEBCVSROOT}/gentoo-x86/
 
-rsync ${RSYNC_ARGS} cvs.gentoo.org:/home/cvsroot/gentoo-x86/ \
-${WORKDIR}/cvsroot/gentoo-x86/
-
-rsync ${RSYNC_ARGS} cvs.gentoo.org:/home/cvsroot/gentoo-src/ \
-${WORKDIR}/cvsroot/gentoo-src/
-
-#done
+	rsync ${RSYNC_ARGS} cvs.gentoo.org:/home/cvsroot/gentoo-src/ \
+	${WEBCVSROOT}/gentoo-src/
+else
+	echo "Either WEBCVSROOT isn't set or it isn't a directory" >&2
+	exit 1
+fi
