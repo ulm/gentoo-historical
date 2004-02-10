ae3ffb28d81d053b4139b7d487486ee3389ec55b
Eric Sammer <esammer@gentoo.org>
2004-02-10 00:24:32 +0000

    added super skeletal GLI module.

---
+++ src/installer/src/GLI.py
@@ -0,0 +1,23 @@
+"""
+Gentoo Linux Installer
+
+$Id: GLI.py,v 1.1 2004/02/10 00:24:32 esammer Exp $
+Copyright 2004 Gentoo Technologies Inc.
+
+The GLI module contains all classes used in the Gentoo Linux Installer (or GLI).
+"""
+
+class InstallProfile(object):
+	"""
+	An object representation of a profile.
+
+	InstallProfile is an object representation of a parsed installation
+	profile file.
+	"""
+
+	def __init__(self):
+		pass
+	
+	def parse(self, path):
+		pass
+
