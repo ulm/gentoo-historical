33d5167872b353ab67a410e7d40d33ad60aa1b8c
Eric Sammer <esammer@gentoo.org>
2004-02-15 02:11:27 +0000

    added cvs and copyright headers to new test files.

--- src/installer/src/tests/test_GLI_InstallProfile.py
+++ src/installer/src/tests/test_GLI_InstallProfile.py
@@ -1,3 +1,8 @@
+"""
+Gentoo Linux Installer Test Suite
+Copyright 2004 Gentoo Technologies, Inc.
+$Header: /var/cvsroot/gentoo/src/installer/src/tests/test_GLI_InstallProfile.py,v 1.2 2004/02/15 02:11:27 esammer Exp $
+"""
 
 import unittest
 import os
