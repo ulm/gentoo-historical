6a7883f7d4ba6527789ba65cda67dc3ac6b5d30f
Eric Sammer <esammer@gentoo.org>
2004-04-09 02:46:59 +0000

    updated unit test file to actually work.

--- src/installer/src/tests/test_GLI_InstallProfile.py
+++ src/installer/src/tests/test_GLI_InstallProfile.py
@@ -1,25 +1,25 @@
 """
 Gentoo Linux Installer Test Suite
 Copyright 2004 Gentoo Technologies, Inc.
-$Header: /var/cvsroot/gentoo/src/installer/src/tests/test_GLI_InstallProfile.py,v 1.3 2004/02/15 20:34:09 esammer Exp $
+$Header: /var/cvsroot/gentoo/src/installer/src/tests/test_GLI_InstallProfile.py,v 1.4 2004/04/09 02:46:59 esammer Exp $
 """
 
 import unittest
 import os
-import GLI
+import GLIInstallProfile
 
 class test_GLIInstallProfile (unittest.TestCase):
 
 	def setUp(self):
-		self.failUnless(os.path.exists("GLI.py"), "Please run tests from src")
+		self.failUnless(os.path.exists("GLIInstallProfile.py"), "Please run tests from src")
 
 	def testInstantiate(self):
-		profile = GLI.InstallProfile();
+		profile = GLIInstallProfile.InstallProfile();
 
 		self.failUnless(profile, "Could not instantiate InstallProfile");
 
 	def testParse(self):
-		profile = GLI.InstallProfile()
+		profile = GLIInstallProfile.InstallProfile()
 
 		self.failUnless(profile, "Could not instantiate InstallProfile");
 
