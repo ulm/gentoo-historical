528c2d0cdf8ebe2e303f0f599eeb5e25122a196f
John P. Davis <zhen@gentoo.org>
2004-04-12 14:38:26 +0000

    lots of changes to the bash backend

---
+++ src/catalyst/targets/livecd-stage1/livecd-stage1-chroot.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Copyright 1999-2003 Gentoo Technologies, Inc.
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/livecd-stage1/Attic/livecd-stage1-chroot.sh,v 1.1 2004/04/12 14:38:26 zhen Exp $
+
+/usr/sbin/env-update
+source /etc/profile
+
+if [ -n "${clst_ENVSCRIPT}" ]
+then
+	source /tmp/envscript
+	rm -f /tmp/envscript
+fi
+
+if [ -n "${clst_CCACHE}" ]
+then
+	export clst_myfeatures="${clst_myfeatures} ccache"
+	emerge --oneshot --nodeps ccache || exit 1
+fi
+
+if [ -n "${clst_DISTCC}" ]
+then   
+	export clst_myfeatures="${clst_myfeatures} distcc"
+	export DISTCC_HOSTS="${clst_distcc_hosts}"
+
+	USE="-gnome -gtk" emerge --oneshot --nodeps distcc || exit 1
+	echo "distcc:x:240:2:distccd:/dev/null:/bin/false" >> /etc/passwd
+	/usr/bin/distcc-config --install 2>&1 > /dev/null
+	/usr/bin/distccd 2>&1 > /dev/null
+fi
+
+if [ -n "${clst_PKGCACHE}" ]
+then
+	clst_emergeopts="--usepkg --buildpkg"
+else
+	clst_emergeopts=""
+fi
+
+## setup the environment
+export FEATURES="${clst_myfeatures}"
+export CONFIG_PROTECT="-*"
+
+## START BUILD
+USE="build" emerge portage
+#turn off auto-use:
+export USE_ORDER="env:conf:defaults"	
+
+emerge ${clst_emergeopts} ${clst_packages}
