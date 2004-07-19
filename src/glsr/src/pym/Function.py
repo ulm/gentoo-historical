486863bf39ef22a8368324ac3c231c8d2aa758b5
Scott Hadfield <hadfield@gentoo.org>
2004-07-19 00:54:47 +0000

    Added dprint() function for printing statements used for debugging

--- src/glsr/src/pym/Function.py
+++ src/glsr/src/pym/Function.py
@@ -2,10 +2,12 @@
 # Copyright 1999-2004 Gentoo Technologies, Inc.
 # Distributed under the terms of the GNU General Public License v2
 #
-# $Id: Function.py,v 1.5 2004/07/07 01:39:57 port001 Exp $
+# $Id: Function.py,v 1.6 2004/07/19 00:54:47 hadfield Exp $
 #
 
 from time import time
+
+import Config
 
 __modulename__ = "Function"
 
@@ -20,3 +22,16 @@ def stop_timer():
 def eval_timer(start, end):
 
     return float(end - start)
+
+def dprint(str):
+    """ Used for printing debug statements to the screen """
+    
+    if not Config.Debug:
+        return True
+
+    if Config.HTMLHeadersSent == False:
+        print "Content-type:text/html\n\n"
+        Config.HTMLHeadersSent = True
+
+    print "DEBUG:%s\n" % str
+    return True
