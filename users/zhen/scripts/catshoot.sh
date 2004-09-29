110624b1a52669bef48a45facafbe92d91660e50
John P. Davis <zhen@gentoo.org>
2004-09-29 06:35:16 +0000

    removing cruft

--- users/zhen/scripts/catshoot.sh
+++ users/zhen/scripts/catshoot.sh
@@ -4,14 +4,10 @@
 
 BASEDIR="/usr/src/gentoo/gentoo/users/zhen"
 NICEDATE="`date +%Y%m%d`"
-PROFILE="hardened-x86-1.4"
-
-cd ${BASEDIR}/profiles
-tar cjf /tmp/${PROFILE}-${NICEDATE}.tar.bz2 ${PROFILE}
 
 cd ${BASEDIR}
 tar cjf /tmp/catalyst-${NICEDATE}.tar.bz2 --exclude CVS catalyst
 
-scp /tmp/${PROFILE}-${NICEDATE}.tar.bz2 /tmp/catalyst-${NICEDATE}.tar.bz2 zhen@dev.gentoo.org:public_html/Hardened/snaps
+scp /tmp/catalyst-${NICEDATE}.tar.bz2 zhen@dev.gentoo.org:public_html/Hardened/snaps
 
-rm /tmp/${PROFILE}-${NICEDATE}.tar.bz2 /tmp/catalyst-${NICEDATE}.tar.bz2
+rm /tmp/catalyst-${NICEDATE}.tar.bz2
