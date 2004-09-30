f96888de32d922af278a91841ab54404f6fdcb8b
Scott Hadfield <hadfield@gentoo.org>
2004-09-30 03:03:46 +0000

    Cleaned up the code a little. No actual changes.

--- src/glsr/src/pym/Logging.py
+++ src/glsr/src/pym/Logging.py
@@ -2,7 +2,7 @@
 # Copyright 1999-2004 Gentoo Technologies, Inc.
 # Distributed under the terms of the GNU General Public License v2
 #
-# $Id: Logging.py,v 1.7 2004/08/22 23:23:39 hadfield Exp $
+# $Id: Logging.py,v 1.8 2004/09/30 03:03:46 hadfield Exp $
 #
 
 import traceback
@@ -13,13 +13,17 @@ import Config
 __modulename__ = "Logging"
 
 def err(msg, modname):
-    """Send given error message to logwrite(), display error message to
-    user then report the error"""
+    """Displays and records errors.
+
+    Send given error message to logwrite(), display error message to
+    user then report the error.
+    """
 
     if Config.ErrorReporting == True:
         try:
             fd = open(Config.ErrorReportLog, "a")
-            fd.write("%s||%s||%s\n" % (strftime("%d %b %Y %H:%M:%S", gmtime()), modname, msg))
+            fd.write("%s||%s||%s\n" % (strftime("%d %b %Y %H:%M:%S", gmtime()),
+                                       modname, msg))
             fd.close()
         except:
             pass
@@ -30,67 +34,69 @@ def err(msg, modname):
         print "Content-type:text/html\n\n"
         Config.HTMLHeadersSent = True
 
+    output = ("""
+    <table align="center" width="90%">
+      <tr>
+        <td align="left">
+          <br />""")
+    
     if Config.Debug == True:
-        print ("<table align=\"center\" width=\"90%\">\n" +
-                  "<tr>\n" +
-                     "<td align=\"left\">\n" +
-                     "<br />\n" +
-                       "<font color=\"FF0000\"><b>Debug Mode</b></font>\n" +
-                       "<br /><br />\n" +
-                       "<b>Internal Error</b> (in module '%s')<b>:</b>\n" % modname +
-                       "<br /><br />\n" +
-                       "%s\n" % msg +
-                       "<br /><br />\n" +
-                       "<b>Traceback:</b>\n" +
-                       "<br /><br />\n")
+        output += ("""
+          <font color="FF0000"><b>Debug Mode</b></font>
+          <br /><br />""")
+
+        output += "<b>Internal Error</b> (in module '%s')<b>:</b>\n" % modname
+        output += "<br /><br />\n%s\n" % msg
+        output += ("""
+           <br /><br />
+           <b>Traceback:</b>
+           <br /><br />""")
 
         tb = traceback.format_stack(None)
         for line in tb[:-1]:
             print line.replace("\n", "<br>")
         
         if modname == "Template":
-            print ("<br />\n" +
-                    "<b>Template module recursion averted.</b>\n" +
-                   "<br /><br />\n")
+            output += ("""
+            <br />
+            <b>Template module recursion averted.</b>
+            <br /><br />""")
 
-            print ("     <br />\n" +
-                     "</td>\n" +
-                  "</tr>\n" +
-                "</table>\n")
-        
     else:
-        time = strftime("%d/%b/%Y %H:%M:%S", gmtime())
+        cur_time = strftime("%d/%b/%Y %H:%M:%S", gmtime())
 
-        print ("<table align=\"center\" width=\"90%\">\n" +
-                   "<tr>\n" +
-                      "<td align=\"left\">\n" +
-                       "<br />\n" +
-                       "<b>Ooops!</b>\n" + 
-                       "<br /><br />\n" +
-                       "It looks like you've encountered an internal error!\n" +
-                       "<br /><br />\n")
+        output += ("""
+           <b>Ooops!</b>
+           <br /><br />
+           It looks like you've encountered an internal error!
+           <br /><br />""")
         
         if Config.ErrorReporting == True:
-            print "This error has been reported to the administration.\n"
+            output += "This error has been reported to the administration.\n"
+        
         else:              
-            print "Please contact <b>%s</b> and quote the time '<b>%s</b>'" % (Config.Contact, time)
+            output += ("Please contact <b>%s</b> and quote the time " %
+                       Config.Contact + "'<b>%s</b>'" % cur_time)
 
-        print ("     <br />\n" +
-                    "</td>\n" +
-                  "</tr>\n" +
-               "</table>\n")
+    output += ("""
+           <br />
+         </td>
+       </tr>
+     </table>""")
 
+    print output
+    
     # Only print the footer if we think the header got printed.
-    if Config.HTMLHeadersSent == True:
-        # Don't print the footer if the error came from the Template module.
-        if modname != "Template":
-            import Template as TemplateHandler
+    # And don't print the footer if the error came from the Template module.
+    if Config.HTMLHeadersSent == True and modname != "Template":
+        import Template as TemplateHandler
 
-            FooterTemplate = TemplateHandler.Template()
-            FooterTemplate.compile(Config.Template["footer"],
-                                   {"GLSR_VERSION":     Config.Version,
-                                    "CONTACT":          Config.Contact})
-            print FooterTemplate.output()
+        FooterTemplate = TemplateHandler.Template()
+        FooterTemplate.compile(Config.Template["footer"],
+                               {"GLSR_VERSION":     Config.Version,
+                                "CONTACT":          Config.Contact})
+        print FooterTemplate.output()
+
     
 def FlushErrorReportLog():
     """Blank the ErrorReport log"""
@@ -123,8 +129,9 @@ def logwrite(msg, modname, type):
 
         try:
             fd = open(Config.LogFile, "a")
-            fd.write("[%s] [%s] [%s] %s\n" % (strftime("%d %b %Y %H:%M:%S", gmtime()),
-                                              logtype, modname, msg))
+            fd.write("[%s] [%s] [%s] %s\n" %
+                     (strftime("%d %b %Y %H:%M:%S", gmtime()),
+                      logtype, modname, msg))
             fd.close()
         except:
             pass
@@ -132,7 +139,6 @@ def logwrite(msg, modname, type):
 def ReturnErrorReports():
 
     Reports = []
-    row = "even"
 
     try:
         fd = open(Config.ErrorReportLog, "r")
@@ -141,15 +147,19 @@ def ReturnErrorReports():
     except:
         return False
 
+    row = "even"
     for line in contents:
+        
         if line == "\n":
             continue
 
         try:
             (date, module, error) = line.split("||", 2)
-            Reports.append({"row": row, "date": date, "module": module, "error": error.strip()})
+            Reports.append({"row": row, "date": date, "module": module,
+                            "error": error.strip()})
         except:
-            Reports.append({"row": row, "date": "N/A", "module": "N/A", "error": "Corrupt report"})
+            Reports.append({"row": row, "date": "N/A", "module": "N/A",
+                            "error": "Corrupt report"})
 
         if row == "even":
             row = "odd"
