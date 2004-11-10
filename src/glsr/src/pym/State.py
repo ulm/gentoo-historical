622a99e72d732ffeab9356951568833b9f3bdeb1
Ian Leitch <port001@gentoo.org>
2004-11-10 10:26:39 +0000

    New State module

---
+++ src/glsr/src/pym/State.py
@@ -0,0 +1,11 @@
+# Copyright 2004 Ian Leitch
+# Copyright 2004 Scott Hadfield
+# Copyright 1999-2004 Gentoo Technologies, Inc.
+# Distributed under the terms of the GNU General Public License v2
+#
+# $Id: State.py,v 1.1 2004/11/10 10:26:39 port001 Exp $
+#
+
+__modulename__ = "State"
+
+ActiveThreads = 0
