ee2bc3329c02bbb831768f91d71efba082fc671e
Mike Frysinger <vapier@gentoo.org>
2004-11-17 01:52:42 +0000

    modularize the code and add support for mips/cobalt machines in the process

--- src/lcdsplash/splash-functions.sh
+++ src/lcdsplash/splash-functions.sh
@@ -1,26 +1,35 @@
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/lcdsplash/splash-functions.sh,v 1.1 2004/10/14 04:40:51 vapier Exp $
+# $Header: /var/cvsroot/gentoo/src/lcdsplash/splash-functions.sh,v 1.2 2004/11/17 01:52:42 vapier Exp $
 
+lcddir="/lib/rcscripts/lcdsplash"
 source /etc/lcdsplash.conf
+
+lcdsplash_start()    { :;}
+lcdsplash_stop()     { :;}
+lcdsplash_started()  { :;}
+lcdsplash_stopped()  { :;}
+lcdsplash_init()     { :;}
+lcdsplash_exit()     { :;}
+lcdsplash_critical() { :;}
+
+if [ -n "${LCD_MODULE}" ] && [ -e "${lcddir}/${LCD_MODULE}" ] ; then
+	source "${lcddir}/${LCD_MODULE}"
+fi
 
 splash() {
 	local func="$1"
 	shift
 
 	case "${func}" in
-		svc_start)   to_lcd "->$1";;
-		svc_stop)    to_lcd "<-$1";;
-		svc_started) to_lcd ">>$1";;
-		svc_stopped) to_lcd "<<$1";;
-		rc_init)     to_lcd "INIT: $1";;
-		rc_exit)     to_lcd $'\x0';;
-		critical)    to_lcd "!!$*";;
+		svc_start)   lcdsplash_start "$@";;
+		svc_stop)    lcdsplash_stop "$@";;
+		svc_started) lcdsplash_started "$@";;
+		svc_stopped) lcdsplash_stopped "$@";;
+		rc_init)     lcdsplash_init "$@";;
+		rc_exit)     lcdsplash_exit "$@";;
+		critical)    lcdsplash_critical "$@";;
 	esac
 
 	return 0
-}
-
-to_lcd() {
-	echo "$@" > "${LCD_FILE}"
 }
