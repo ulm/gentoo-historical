50b3b80930764b59440276518d215ced345a3e8a
Andrew Gaffney <agaffney@gentoo.org>
2004-12-11 06:29:01 +0000

    NetworkMounts with working column sorting

--- src/installer/src/fe/gtk/NetworkMounts.py
+++ src/installer/src/fe/gtk/NetworkMounts.py
@@ -4,22 +4,29 @@ import GLIScreen
 class Panel(GLIScreen.GLIScreen):
 
 	title = "Network Mounts"
+	columns = []
 
 	def __init__(self, controller):
 		GLIScreen.GLIScreen.__init__(self, controller)
 		vert = gtk.VBox(gtk.FALSE, 0)
 		vert.set_border_width(10)
 
-#		self.treedata = gtk.TreeStore(gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING)
 		self.treedata = gtk.ListStore(gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING)
 		self.treedata.append(["kagome", "/usr/portage", "NFS", "/usr/portage", "ro"])
 		self.treedata.append(["kagome", "/var/cache/edb/dep", "NFS", "/var/cache/edb/dep", "rw,noauto"])
-		self.treeview = gtk.TreeView(self.treedata)
-		self.treeview.append_column(gtk.TreeViewColumn("Host/IP", gtk.CellRendererText(), text=0))
-		self.treeview.append_column(gtk.TreeViewColumn("Export/Share", gtk.CellRendererText(), text=1))
-		self.treeview.append_column(gtk.TreeViewColumn("Type", gtk.CellRendererText(), text=2))
-		self.treeview.append_column(gtk.TreeViewColumn("Mount Point", gtk.CellRendererText(), text=3))
-		self.treeview.append_column(gtk.TreeViewColumn("Mount Options", gtk.CellRendererText(), text=4))
+		self.treedatasort = gtk.TreeModelSort(self.treedata)
+		self.treeview = gtk.TreeView(self.treedatasort)
+		self.columns.append(gtk.TreeViewColumn("Host/IP", gtk.CellRendererText(), text=0))
+		self.columns.append(gtk.TreeViewColumn("Export/Share", gtk.CellRendererText(), text=1))
+		self.columns.append(gtk.TreeViewColumn("Type", gtk.CellRendererText(), text=2))
+		self.columns.append(gtk.TreeViewColumn("Mount Point", gtk.CellRendererText(), text=3))
+		self.columns.append(gtk.TreeViewColumn("Mount Options", gtk.CellRendererText(), text=4))
+		col_num = 0
+		for column in self.columns:
+			column.set_resizable(gtk.TRUE)
+			column.set_sort_column_id(col_num)
+			self.treeview.append_column(column)
+			col_num += 1
 		self.treewindow = gtk.ScrolledWindow()
 		self.treewindow.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
 		self.treewindow.add(self.treeview)
