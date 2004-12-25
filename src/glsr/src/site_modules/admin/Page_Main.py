011a81b5f81bc48f6b29940d9ddcd958d99b0e81
Ian Leitch <port001@gentoo.org>
2004-12-25 21:35:00 +0000

    More fastcgi migration

--- src/glsr/src/site_modules/admin/Page_Main.py
+++ src/glsr/src/site_modules/admin/Page_Main.py
@@ -3,67 +3,66 @@
 # Copyright 1999-2004 Gentoo Technologies, Inc.
 # Distributed under the terms of the GNU General Public License v2
 #
-# $Id: Page_Main.py,v 1.11 2004/11/14 23:34:23 port001 Exp $
+# $Id: Page_Main.py,v 1.12 2004/12/25 21:35:00 port001 Exp $
 #
 
+import State
 import Config
-import Session
 from User import User
+from Session import Session
 from site_modules import SiteModule, Redirect
 from Admin import optimize_tables, get_optimize_days
 from Logging import ReturnErrorReports, FlushErrorReportLog
 
 class Page_Main(SiteModule):
 
-    __modulename__ = "Page_Main"
+    __modulename__ = 'Page_Main'
 
-    def __init__(self, **args):
+    def __init__(self, req, **args):
 
-        self.pages = [None, "main"]
-        self.form = args["form"]
-        self.page = self.form.getvalue("page")
-        self.template = Config.Template["admin_main"]
-    
-        self.ThisSession = Session.New() 
+        self.req = req
+
+        self.page = args['page']
+        self.template = Config.Template['admin_main']
     
     def _set_params(self):
 
         users_online_list = []
-        row = "even"
+        row = 'even'
 
         # Count active users
-        active_sessions = self.ThisSession.ListSessionsOnline(Config.WhoIsOnlineOffset)
+        active_sessions = Session.get_active(Config.WhoIsOnlineOffset)
 
         if len(active_sessions) == 0:
-            self.tmpl.param("USERS_ONLINE", "False")
-            self.tmpl.param("USERS_ONLINE_LIST", [], "loop")
+            self.tmpl.param('USERS_ONLINE', 'False')
+            self.tmpl.param('USERS_ONLINE_LIST', [], 'loop')
         else:
-            self.tmpl.param("USERS_ONLINE", "True")
+            self.tmpl.param('USERS_ONLINE', 'True')
             for session in active_sessions:
-                ThisUser = User(session["session_user_id"])
+                ThisUser = User(session['session_user_id'])
                 alias = ThisUser.GetAlias()
                 ip = ThisUser.GetLastIP()
 
                 if alias == False:
-                    alias = "N/A"
+                    alias = 'N/A'
                 elif ip == False:
-                    ip = "N/A"
+                    ip = 'N/A'
 
-                users_online_list.append({"row": row,
-                                          "user": alias,
-                                          "ip": ip})
-                if row == "even":
-                    row = "odd"
+                users_online_list.append({'row': row,
+                                          'user': alias,
+                                          'ip': ip})
+                if row == 'even':
+                    row = 'odd'
                 else:
-                    row = "even"
+                    row = 'even'
 
-            self.tmpl.param("USERS_ONLINE_LIST", users_online_list, "loop")
+            self.tmpl.param('USERS_ONLINE_LIST', users_online_list, 'loop')
 
         if Config.ErrorReporting == True:
             list_offset = int()
-            if self.form.has_key("error_report_offset"):
-                list_offset = self.form.getvalue("error_report_offset")
-                if str(list_offset).lower() == "all":
+            if self.req.values('error_report_offset') != None:
+                list_offset = self.req.values('error_report_offset')
+                if str(list_offset).lower() == 'all':
                     list_offset = -1
                 else:
                     list_offset = int(list_offset)
@@ -71,24 +70,24 @@ class Page_Main(SiteModule):
                 list_offset = Config.ErrorReportListOffset
             error_report_list = ReturnErrorReports(list_offset)
             if error_report_list == False:
-                self.tmpl.param("ERROR_REPORTING", "False")
-                self.tmpl.param("ERROR_REPORT_LIST", [], "loop")
+                self.tmpl.param('ERROR_REPORTING', 'False')
+                self.tmpl.param('ERROR_REPORT_LIST', [], 'loop')
             else:
-                self.tmpl.param("ERROR_REPORTING", "True")
-                self.tmpl.param("ERROR_REPORT_LIST", error_report_list, "loop")
+                self.tmpl.param('ERROR_REPORTING', 'True')
+                self.tmpl.param('ERROR_REPORT_LIST', error_report_list, 'loop')
         else:
-            self.tmpl.param("ERROR_REPORTING", "False")
-            self.tmpl.param("ERROR_REPORT_LIST", [], "loop")
+            self.tmpl.param('ERROR_REPORTING', 'False')
+            self.tmpl.param('ERROR_REPORT_LIST', [], 'loop')
 
-        self.tmpl.param("OPTIMIZE_DAYS", get_optimize_days())
-        self.tmpl.param("GLSR_URL", Config.URL)
+        self.tmpl.param('OPTIMIZE_DAYS', get_optimize_days())
+        self.tmpl.param('GLSR_URL', Config.URL)
 
     def _select_action(self):
 
-        if self.form.getvalue("flush_error_reports"):
+        if self.req.values('flush_error_reports'):
             FlushErrorReportLog()
 
-        if self.form.getvalue("optimize_tables"):
+        if self.req.values('optimize_tables'):
             optimize_tables()
 
     def display(self):
--- src/glsr/src/site_modules/admin/Page_Statistics.py
+++ src/glsr/src/site_modules/admin/Page_Statistics.py
@@ -3,7 +3,7 @@
 # Copyright 1999-2004 Gentoo Technologies, Inc.
 # Distributed under the terms of the GNU General Public License v2
 #
-# $Id: Page_Statistics.py,v 1.3 2004/12/16 15:43:31 port001 Exp $
+# $Id: Page_Statistics.py,v 1.4 2004/12/25 21:35:00 port001 Exp $
 #
 
 import Stat
@@ -12,23 +12,24 @@ from site_modules import SiteModule
 
 class Page_Statistics(SiteModule):
 
-    __modulename__ = "Page_Statistics"
+    __modulename__ = 'Page_Statistics'
 
-    def __init__(self, **args):
+    def __init__(self, req, **args):
 
-        self.pages = ["stat"]
-        self.template = Config.Template["admin_statistics"]
+        self.req = req
+
+        self.template = Config.Template['admin_statistics']
 
     def _set_params(self):
 
-        self.tmpl.param("GLSR_URL", Config.URL)
-        self.tmpl.param("USER_COUNT", Stat.UserCount())
-        self.tmpl.param("SESSION_COUNT", Stat.SessionCount())
-        self.tmpl.param("SCRIPT_COUNT", Stat.ScriptCount())
-        self.tmpl.param("SUBSCRIPT_COUNT", Stat.SubScriptCount())
-        self.tmpl.param("CATEGORY_COUNT", Stat.CategoryCount())
-        self.tmpl.param("COMMENT_COUNT", Stat.CommentCount())
-        self.tmpl.param("DBSIZE", Stat.DBSize())
+        self.tmpl.param('GLSR_URL', Config.URL)
+        self.tmpl.param('USER_COUNT', Stat.UserCount())
+        self.tmpl.param('SESSION_COUNT', Stat.SessionCount())
+        self.tmpl.param('SCRIPT_COUNT', Stat.ScriptCount())
+        self.tmpl.param('SUBSCRIPT_COUNT', Stat.SubScriptCount())
+        self.tmpl.param('CATEGORY_COUNT', Stat.CategoryCount())
+        self.tmpl.param('COMMENT_COUNT', Stat.CommentCount())
+        self.tmpl.param('DBSIZE', Stat.DBSize())
 
     def display(self):
 
