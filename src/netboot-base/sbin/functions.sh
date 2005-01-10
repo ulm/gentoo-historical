cd566706ccba406a37b4031d722e8f8c92906fe7
Guy Martin <gmsoft@gentoo.org>
2005-01-10 14:22:22 +0000

    Various fixes in rc scripts and functions.

--- src/netboot-base/sbin/functions.sh
+++ src/netboot-base/sbin/functions.sh
@@ -1,6 +1,6 @@
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/netboot-base/sbin/functions.sh,v 1.2 2004/10/07 20:37:53 vapier Exp $
+# $Header: /var/cvsroot/gentoo/src/netboot-base/sbin/functions.sh,v 1.3 2005/01/10 14:22:22 gmsoft Exp $
 
 # Stripped down version of gentoo-src/rc-scripts/sbin/functions.sh
 
@@ -17,6 +17,14 @@ if [ -r /proc/cmdline ]
 then
 	grep -q nocolor /proc/cmdline && RC_NOCOLOR="yes"
 fi
+
+# This function if required to kill devfsd and busybox because busybox
+# think the executable name is busybox.
+my_pidof() {
+	[ "${1}" = "" ] && return
+	ps aux | grep "${1}" | grep -v grep | awk '{ print $1 }'
+
+}
 
 eerror() {
 	if [ "${RC_QUIET_STDOUT}" = "yes" ]
@@ -72,14 +80,14 @@ eend() {
 	return 0
 }
 
-if [ "${RC_ENDCOL}" == "yes" ] ; then
+if [ "${RC_ENDCOL}" = "yes" ] ; then
 	COLS=${COLUMNS:-0}		# bash's internal COLUMNS variable
-	(( COLS == 0 )) && COLS=$(stty size | cut -d' ' -f2)
-	(( COLS -= 7 ))			# width of [ ok ]
-	if (( COLS <= 0 )); then
+	[ ${COLS} -eq 0 ] && COLS=$(stty size | cut -d' ' -f2)
+	COLS=$(( ${COLS} - 7 ))			# width of [ ok ]
+	if [ ${COLS} -le 0 ]; then
 		# Sanity check ... with serial consoles and such,
 		# COLS may up being negative ...
-		(( COLS = 80 ))
+		COLS=80
 	fi
 	ENDCOL=$'\e[A\e['${COLS}'G'
 else
