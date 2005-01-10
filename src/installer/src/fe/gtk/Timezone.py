9d25d63d700404689ed36dc10b6eb068ea46c4f5
Andrew Gaffney <agaffney@gentoo.org>
2005-01-10 00:40:31 +0000

    updates from allanonjl

--- src/installer/src/fe/gtk/Timezone.py
+++ src/installer/src/fe/gtk/Timezone.py
@@ -22,7 +22,7 @@ class Panel(GLIScreen.GLIScreen):
 
         content_str = """
 This is where you select what timezone you are in.
-[short explanation here]
+[You must press enter or double click to set the timezone!]
 """
  	
 	# pack the description
@@ -46,8 +46,17 @@ This is where you select what timezone you are in.
 		#piter2=self.treestore.append(piter, ['child %i of parent %i' % (child, parent)])
 		#for child2 in range(2):
 		    #self.treestore.append(piter2, ['child2 %i of child %i' % (child2, child)])
+	# add the box to tell you what timezone you selected
+	label=gtk.Label("Your Selection: ")
+	self.label2=gtk.Label("")
+	selection = widgets.hBoxThese(gtk.FALSE,10,[label,self.label2])
+	selection = widgets.hBoxThese(gtk.TRUE,10,[selection])
+	vert.pack_start(selection,expand=gtk.FALSE,fill=gtk.FALSE,padding=0)
 	
 	self.treeview = gtk.TreeView(self.treestore)
+	self.treeview.connect("select-cursor-row",self.callback2,self.treeview.get_selection())
+	self.treeview.connect("row-activated",self.callback2)
+	self.selected_path=[]
 	self.tvcolumn = gtk.TreeViewColumn('Please Select A Timezone: ')
 	self.treeview.append_column(self.tvcolumn)
 	self.cell = gtk.CellRendererText()
@@ -65,8 +74,37 @@ This is where you select what timezone you are in.
     	self.add_content(vert)
 
     # call back for when they change the timezone
-    def callback2(self,widget,data=None):
-	print "Selected %s" % (data)
+    def callback2(self,widget,data=None,test=None):
+	self.selected_path=[]
+	final_path=""
+	#print "Selected %s" % (self.treeview.get_value(test[1],0))
+	entry1, entry2 = self.treeview.get_selection().get_selected()
+	# if it doesn't have children, do the following:
+	if(entry1.iter_children(entry2)==None):
+	    entry = entry1.get_value(entry2, 0)
+	    entry5 = entry1.get_path(entry2)
+	    #print entry5
+	    for count in range(len(entry5)):
+		# first select the path
+		self.treeview.get_selection().select_path(entry5[0:len(entry5)-count])
+		# get the text
+		entry1, entry2= self.treeview.get_selection().get_selected()
+		entry = entry1.get_value(entry2, 0)
+		# add to the list
+		self.selected_path.append(entry)
+	    
+	    self.selected_path.reverse()
+	    #print self.selected_path
+	    
+	    for item in self.selected_path:
+		final_path=final_path+"/"+item
+	    print "You Selected: "+final_path
+	    
+	    # update the selection box
+	    self.label2.set_label(final_path)
+	    # set it in the profile
+	    self.controller.install_profile.set_time_zone(None, "/usr/local/zoneinfo"+final_path, None)
+	
 
     # figures out which one is selected
     def whichSelected(self,data):
@@ -120,6 +158,14 @@ This is where you select what timezone you are in.
 	    self.append_to_treestore(depth+1)
 
     def activate(self):
+	# grab from the install profile
+	timezone=self.controller.install_profile.get_time_zone()
+	print "Timezone: " + timezone
+	
+	# Set the label ( strip off the /usr/share/zoneinfo prefix )
+	timezone=timezone[19:]
+	self.label2.set_label(timezone)
+	
 	self.controller.SHOW_BUTTON_EXIT    = gtk.TRUE
 	self.controller.SHOW_BUTTON_HELP    = gtk.TRUE
 	self.controller.SHOW_BUTTON_BACK    = gtk.TRUE
