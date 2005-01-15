a75b2d10fa925750678db3242d8948a781c6d97a
Andrew Gaffney <agaffney@gentoo.org>
2005-01-15 08:08:22 +0000

    USE editor in MakeDotConf.py

--- src/installer/src/fe/gtk/MakeDotConf.py
+++ src/installer/src/fe/gtk/MakeDotConf.py
@@ -1,67 +1,111 @@
 import gtk
 import GLIScreen
-from Widgets import Widgets
+import commands
+import gobject
 
 class Panel(GLIScreen.GLIScreen):
-    """
-    The make.conf section of the installer.
-    
-    @author:    John N. Laliberte <allanonl@bu.edu>
-    @license:   GPL
-    """
-    # Attributes:
-    title="Make.conf Settings"
-    # Operations
-    # commands.getoutput("source /etc/make.conf; echo $VALUE_YOU_WANT")
-    # blah = GLIUtility.get_value_from_config(filename, value)
-    def __init__(self, controller):
-	GLIScreen.GLIScreen.__init__(self, controller)
 
-        vert    = gtk.VBox(gtk.FALSE, 10) # This box is content so it should fill space to force title to top
-	horiz   = gtk.HBox(gtk.FALSE, 10)
+	title = "make.conf"
+	make_conf_values = {}
+	use_flags = []
+	use_desc = {}
+	columns = []
 
-        content_str = """
-This is where you setup make.conf.
-[short explanation of what make.conf is here]
+	def __init__(self, controller):
+		GLIScreen.GLIScreen.__init__(self, controller)
+
+		self.system_use_flags = commands.getoutput("emerge info | grep -e '^USE' | sed -e 's:USE=\"::' -e 's:\"::'").split(" ")
+
+		vert = gtk.VBox(gtk.FALSE, 0)
+		vert.set_border_width(10)
+
+		content_str = """On this screen, you'll define all of your /etc/make.conf settings.
 """
-	# pack the description
-	vert.pack_start(gtk.Label(content_str), expand=gtk.FALSE, fill=gtk.FALSE, padding=10)
 
-	widgets=Widgets()
-	
-	makeoptions=["ACCEPT_KEYWORDS","CFLAGS","CHOST","MAKEOPTS","FEATURES","USE"]
-	self.makeoptions=makeoptions
-	self.makeoptions_widgets=[]
-	
-	for makeoption in makeoptions:
-	 new_box=widgets.textBox2(self,26,makeoption)
-	 self.makeoptions_widgets.append(new_box)
-	 new_box=widgets.labelIt(makeoption,new_box)
-	 new_box=widgets.hBoxIt(new_box)
-	 vert.pack_start(new_box, expand=gtk.FALSE, fill=gtk.FALSE, padding=0)
-	
-    	self.add_content(vert)
+		content_label = gtk.Label(content_str)
+		vert.pack_start(content_label, expand=gtk.FALSE, fill=gtk.FALSE, padding=0)
 
-    def activate(self):
-	make_conf = self.controller.install_profile.get_make_conf()
-	print "From Saved Profile: "+str(make_conf)
-	for item in make_conf:
-	    # if the item in the dictionary is in the list, load it!
-	    if(item in self.makeoptions):
-		number=self.makeoptions.index(item)
-		self.makeoptions_widgets[number].set_text(make_conf[item])
+		f = open("/usr/portage/profiles/use.desc", "r")
+		for line in f:
+			line.strip()
+			if line == "" or line.startswith("#"): continue
+			dash_pos = line.find(" - ")
+			if dash_pos == -1: continue
+			flagname = line[:dash_pos] or line[dash_pos-1]
+			desc = line[dash_pos+3:]
+			self.use_desc[flagname] = desc
+		f.close()
 
-	self.controller.SHOW_BUTTON_EXIT    = gtk.TRUE
-	self.controller.SHOW_BUTTON_HELP    = gtk.TRUE
-	self.controller.SHOW_BUTTON_BACK    = gtk.TRUE
-	self.controller.SHOW_BUTTON_FORWARD = gtk.TRUE
-	self.controller.SHOW_BUTTON_FINISH  = gtk.FALSE
-	
-    def entrycallback(self,widget,data=None):
-	print data.get_text()
-	make_conf = self.controller.install_profile.get_make_conf()
-	make_conf[widget.get_name()]=data.get_text()
-	print "Stored make.conf"
-	print self.controller.install_profile.get_make_conf()
-	self.controller.install_profile.set_make_conf(make_conf)
-	
+		f = open("/usr/portage/profiles/use.local.desc", "r")
+		for line in f:
+			line.strip()
+			if line == "" or line.startswith("#"): continue
+			dash_pos = line.find(" - ")
+			if dash_pos == -1: continue
+			colon_pos = line.find(":", 0, dash_pos)
+			pkg = line[:colon_pos]
+			flagname = line[colon_pos+1:dash_pos] or line[colon_pos+1]
+			desc = "(" + pkg + ") " + line[dash_pos+3:]
+			self.use_desc[flagname] = desc
+		f.close()
+
+		sorted_use = self.use_desc.keys()
+		sorted_use.sort()
+                self.treedata = gtk.ListStore(gobject.TYPE_BOOLEAN, gobject.TYPE_STRING, gobject.TYPE_STRING)
+		for flag in sorted_use:
+                        self.treedata.append([(flag in self.use_flags and self.use_flags[flag] == 1), flag, self.use_desc[flag]])
+                self.treeview = gtk.TreeView(self.treedata)
+		self.toggle_renderer = gtk.CellRendererToggle()
+		self.toggle_renderer.set_property("activatable", True)
+		self.toggle_renderer.connect("toggled", self.flag_toggled)
+		self.columns.append(gtk.TreeViewColumn("Active", self.toggle_renderer, active=0))
+                self.columns.append(gtk.TreeViewColumn("Flag", gtk.CellRendererText(), text=1))
+                self.columns.append(gtk.TreeViewColumn("Description", gtk.CellRendererText(), text=2))
+                for column in self.columns:
+                        column.set_resizable(gtk.TRUE)
+                        self.treeview.append_column(column)
+                self.treewindow = gtk.ScrolledWindow()
+                self.treewindow.set_size_request(-1, 300)
+                self.treewindow.set_shadow_type(gtk.SHADOW_IN)
+                self.treewindow.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+                self.treewindow.add(self.treeview)
+                vert.pack_start(self.treewindow, expand=gtk.FALSE, fill=gtk.FALSE, padding=10)
+ 	
+		self.add_content(vert)
+
+	def flag_toggled(self, cell, path):
+		model = self.treeview.get_model()
+		model[path][0] = not model[path][0]
+		if model[path][0]:
+			self.use_flags['path'] = 1
+		else:
+			if path in self.use_flags: self.use_flags['path'] = 0
+		return
+
+	def activate(self):
+		self.controller.SHOW_BUTTON_EXIT    = gtk.TRUE
+		self.controller.SHOW_BUTTON_HELP    = gtk.TRUE
+		self.controller.SHOW_BUTTON_BACK    = gtk.TRUE
+		self.controller.SHOW_BUTTON_FORWARD = gtk.TRUE
+		self.controller.SHOW_BUTTON_FINISH  = gtk.FALSE
+		self.make_conf_values = self.controller.install_profile.get_make_conf()
+		self.use_flags = {}
+		if not self.make_conf_values.has_key('USE') or self.make_conf_values['USE'] == "":
+			self.make_conf_values['USE'] = self.system_use_flags
+		for flag in self.make_conf_values['USE']:
+			if flag.startswith("-"):
+				flag = flag[1:] or flag[1]
+				self.use_flags[flag] = 0
+			else:
+				self.use_flags[flag] = 1
+		sorted_use = self.use_desc.keys()
+		sorted_use.sort()
+                self.treedata = gtk.ListStore(gobject.TYPE_BOOLEAN, gobject.TYPE_STRING, gobject.TYPE_STRING)
+		for flag in sorted_use:
+                        self.treedata.append([(flag in self.use_flags), flag, self.use_desc[flag]])
+		self.treeview.set_model(self.treedata)
+
+	def deactivate(self):
+		self.make_conf_values['USE'] = " ".join(self.use_flags)
+		set.controller.install_profile.set_make_conf(self.make_conf_values)
+		return True
