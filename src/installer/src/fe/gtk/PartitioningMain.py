fd7aa629087c5b1b903788c5e430159c4a9718db
Andrew Gaffney <agaffney@gentoo.org>
2005-02-17 02:16:11 +0000

    PartitioningMain calls activate() in child screens

--- src/installer/src/fe/gtk/PartitioningMain.py
+++ src/installer/src/fe/gtk/PartitioningMain.py
@@ -6,6 +6,7 @@ import PartitioningUgly
 class Panel(GLIScreen.GLIScreen):
 
 	title = "Partitioning"
+	active_page = 0
 
 	def __init__(self, controller):
 		GLIScreen.GLIScreen.__init__(self, controller)
@@ -17,7 +18,7 @@ class Panel(GLIScreen.GLIScreen):
 		self.notebook.append_page(PartitioningUgly.Panel(self.controller))
 		self.notebook.set_show_tabs(gtk.FALSE)
 		self.notebook.set_show_border(gtk.FALSE)
-		vert.pack_start(self.notebook, expand=gtk.FALSE, fill=gtk.FALSE, padding=0)
+		vert.pack_start(self.notebook, expand=gtk.TRUE, fill=gtk.TRUE, padding=0)
 
 		choices_hbox = gtk.HBox(gtk.FALSE, 0)
 		self.radio_pretty = gtk.RadioButton(group=None, label="Pretty")
@@ -32,6 +33,8 @@ class Panel(GLIScreen.GLIScreen):
 
 	def switch_screen(self, widget, data=None):
 		self.notebook.set_current_page(data)
+		self.notebook.get_nth_page(data).activate()
+		self.active_page = data
 
 	def activate(self):
 		self.controller.SHOW_BUTTON_EXIT    = gtk.TRUE
@@ -39,3 +42,4 @@ class Panel(GLIScreen.GLIScreen):
 		self.controller.SHOW_BUTTON_BACK    = gtk.TRUE
 		self.controller.SHOW_BUTTON_FORWARD = gtk.TRUE
 		self.controller.SHOW_BUTTON_FINISH  = gtk.FALSE
+		self.switch_screen(self.notebook, self.active_page)
