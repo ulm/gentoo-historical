38613526434d08bf1dcc0200dc55d797f03d379d
Andrew Gaffney <agaffney@gentoo.org>
2005-02-18 19:14:16 +0000

    moved lang to __init__

--- src/installer/src/GLILocalization.py
+++ src/installer/src/GLILocalization.py
@@ -1,7 +1,7 @@
 """
 Gentoo Linux Installer
 
-$Id: GLILocalization.py,v 1.1 2005/02/18 16:39:38 agaffney Exp $
+$Id: GLILocalization.py,v 1.2 2005/02/18 19:14:16 agaffney Exp $
 Copyright 2004 Gentoo Technologies Inc.
 
 
@@ -16,20 +16,21 @@ import os
 class Localization:
 
 	messages = None
+	lang = None
 
-	def __init__(self, filename):
+	def __init__(self, filename, lang=None):
 		self.messages = {}
+		self.lang = lang or os.getenv("LANG") # maybe LC_ALL
 		file = codecs.open(filename, "r", "utf-8")
 		for line in file.readlines():
+			line = line.strip()
 			parts = line.split('\t')
 			if not parts[0] in self.messages: self.messages[parts[0]] = {}
 			self.messages[parts[0]][parts[1]] = parts[2]
 
-	def get_localized_message(self, message, lang=None):
+	def get_localized_message(self, message):
 		localized_message = None
 
-		if not lang:
-			lang = os.getenv("LANG") # maybe LC_ALL
-		if message in self.messages and lang in self.messages[message]:
-			localized_message = self.messages[message][lang]
+		if message in self.messages and self.lang in self.messages[message]:
+			localized_message = self.messages[message][self.lang]
 		return localized_message
