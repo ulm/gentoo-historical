709c148fdff10dc4883298376d9ab1ffe64d7f5f
cvs-fast-export <cvs-fast-export>
2005-03-04 16:49:27 +0000

    Synthetic commit for incomplete tag DUBYA-20050118

--- src/catalyst/targets/grp/grp-preclean-chroot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Copyright 1999-2004 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/grp/Attic/grp-preclean-chroot.sh,v 1.6 2005/01/29 14:54:31 wolf31o2 Exp $
-
-/usr/sbin/env-update
-source /etc/profile
