8bdeb49a66d9f63fbcad4658a81ef8f9e01c9ea6
Brian Jackson <iggy@gentoo.org>
2005-03-06 06:29:35 +0000

    some mroe functions that we need to work

--- src/embedded/baselayout-lite/image/sbin/functions.sh
+++ src/embedded/baselayout-lite/image/sbin/functions.sh
@@ -1,17 +1,53 @@
+PATH="/usr/bin:/bin:/usr/sbin:/sbin"
+NO_NET_FS_LIST="noafs,nocifs,nocode,noncpfs,nonfs,nonfs4,noshfs,nosmbfs"
+
 einfo() {
-	echo $*
+	echo "EINFO: $*"
 }
 eerror() {
-	echo $* 
+	echo "EERROR $*"
 }
 ebegin() {
-	echo $*
+	echo "EBEGIN: $*"
+}
+ewarn() {
+	echo "EWARN: $*"
 }
 eend() {
-	echo $*
+	local retval=$1
+	#shift
+	if [ "$retval" -eq "0" ] ; then
+		return $retval
+	fi
+	echo "EEND: $*"
+	return $retval
 }
 
 depend() {
 	return 0
 }
                     
+get_bootparam() {
+    local x copt params retval=1
+
+    [ ! -r "/proc/cmdline" ] && return 1
+
+	if `grep $1 /proc/cmdline` ; then
+		einfo "Found cmdline option: $1"
+		retval=0
+	fi
+
+    return ${retval}
+}
+
+is_net_fs() {
+	grep -q '^[^#]*\/[[:space:]].*nfs' /etc/fstab
+	return $?
+}
+
+is_uml_sys() {
+	[ ! -r "/proc/cpuinfo" ] && return 1
+	grep -q 'UML' /proc/cpuinfo &> /dev/null
+	return $?
+}
+
