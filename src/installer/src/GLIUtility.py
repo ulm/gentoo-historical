7fba23979751d6d0e4c1fd8fb5f0d335916bc2e5
Preston Cody <codeman@gentoo.org>
2005-03-16 15:24:28 +0000

    proper indentation please

--- src/installer/src/GLIUtility.py
+++ src/installer/src/GLIUtility.py
@@ -244,7 +244,7 @@ def spawn(cmd, quiet=False, logfile=None, display_on_tty8=False, chroot=None, ap
 def exitsuccess(status):
 	if os.WIFEXITED(status) and os.WEXITSTATUS(status) == 0:
 		return True
-        print "WIFEXITED = " + str(os.WIFEXITED(status)) + ", WEXITSTSTUS = " + str(os.WEXITSTATUS(status))
+	print "WIFEXITED = " + str(os.WIFEXITED(status)) + ", WEXITSTSTUS = " + str(os.WEXITSTATUS(status))
 
 	return False
 
