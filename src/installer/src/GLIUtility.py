909dd6504da4ccce09e1800a80f8f4c1fd2c50ec
Andrew Gaffney <agaffney@gentoo.org>
2005-03-17 03:59:52 +0000

    spawn() automatically   creates wrapper script for commands run inside chroot so multiple commands   and redirection can be used.

--- src/installer/src/GLIUtility.py
+++ src/installer/src/GLIUtility.py
@@ -218,7 +218,7 @@ def set_default_route(route):
 def spawn(cmd, quiet=False, logfile=None, display_on_tty8=False, chroot=None, append_log=False, return_output=False):
 	# quiet and return_output really do the same thing. One of them need to be removed.
 	if chroot != None:
-		cmd = "chroot " + chroot + " " + cmd
+		cmd = "echo -e '#!/bin/bash\n\nsource /etc/profile\n" + cmd + "' > " + chroot + "/tmp/spawn.sh && chroot " + chroot + " /tmp/spawn.sh"
 
 	if quiet and logfile != None:
 		cmd += " >> " + logfile + " 2>&1"
