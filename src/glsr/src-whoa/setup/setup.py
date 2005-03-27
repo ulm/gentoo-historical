88a45e825e7368fa7d991e6e8f12b2966729a4e5
Scott Hadfield <hadfield@gentoo.org>
2005-03-27 08:46:06 +0000

    simplified and cleaned up the code a bit

--- src/glsr/src-whoa/setup/setup.py
+++ src/glsr/src-whoa/setup/setup.py
@@ -6,6 +6,11 @@
 
 """The GLSR Setup Script.
 
+Sets up the script repository. Performing the following actions:
+  - Creation of web server owned directories
+  - Addition of admin users to the database
+  - Creation of the database and tables
+
 Note that this script will need the root db username and password to create
 the database and the database user. (This isn't actually implemented yet)
 
@@ -15,7 +20,7 @@ TODO:
     least drop them first.
 """
 
-__revision__ = '$Id: setup.py,v 1.1 2005/03/27 04:02:34 hadfield Exp $'
+__revision__ = '$Id: setup.py,v 1.2 2005/03/27 08:46:06 hadfield Exp $'
 __modulename__ = 'setup'
 
 import os
@@ -25,15 +30,13 @@ sys.path.insert(0, "../")
 
 import config
 from core.template import Template as template_handler
-from core.db.mysql import MySQL as db_handler
+from core.db.mysql import SQLdb as db_handler
 
 ###############################################################################
 # Setup DB regular expressions
-# These expressions are used for parsing the mysql.schema file. To add support
-# for an additional database all you need to do is add three new regular
+# These expressions are used for parsing the db.schema file. To add support
+# for an additional database all you need to do is add two new regular
 # expressions for that db.
-# For example, to add support for postgres, you'll need the following regex's:
-# _postgres_db_re, _postgres_table_re, and postgres_table_name_re.
 
 # Create the database handler object.
 _db_obj = db_handler(config.db)
@@ -43,23 +46,14 @@ _mysql_db_re = r'(?is)CREATE\s+DATABASE\s+\w+\s*;'
 
 # Database specific table matching regex's. Used for getting a table definition
 # from the db schema. Note that %s represents the table name.
-_mysql_table_re = r'((?is)CREATE\s+TABLE\s+%s\s*\([^;]*);'
-
-# Database specific table name matching regex's. Used for getting a list of
-# table names in the database schema.
-_mysql_table_name_re = r'(?is)CREATE\s+TABLE\s+([^\s]+)'
+_mysql_table_re = r'(?is)CREATE\s+TABLE\s+(%s)\s*\([^;]*;'
 
 # Set the table name and db matching regex to the appropriate database.
 _db_re = eval("_%s_db_re" % config.db["db_type"])
 _table_re = eval("_%s_table_re" % config.db["db_type"])
-_table_name_re = eval("_%s_table_name_re" % config.db["db_type"])
-
-def str_indent(str_in, indent_amt):
-    
-    indent_str = " " * indent_amt
-    return indent_str + str_in.replace("\n", "\n" + indent_str)
 
 def load_schema(schema_loc, values):
+    """Load and compile the SQL schema template."""
 
     schema_tmpl = template_handler()
     schema_tmpl.param("TBL_PREFIX", config.db["prefix"])
@@ -75,6 +69,7 @@ def load_schema(schema_loc, values):
     return schema_tmpl.output()
 
 def add_user(user_type = ""):
+    """Prompts for user information and returns the info in a dict."""
 
     from getpass import getpass
 
@@ -91,7 +86,7 @@ def add_user(user_type = ""):
         print "ERROR: Zero length password"
         sys.exit(1)
     
-    passwd2 = getpass("Re-type password: ")
+    passwd2 = getpass("Confirm password: ")
     if passwd != passwd2:
         print "ERROR: passwords do not match"
         sys.exit(1)
@@ -108,14 +103,33 @@ def add_user(user_type = ""):
 def _list_tables(schema):
     """Returns a list of table names in the specified schema."""
 
-    return re.findall(_table_name_re, schema)
+    # This assumes that all databases have table names that are alphanumeric
+    tbl_expr = re.compile(_table_re % "\w+")
+    
+    table_list = []
+    start_pos = 0
+    
+    while True:
+        # Loop through the schema to find all of the table names.
+        # match.group(0) will match the entire table definition.
+        # match.group(1) will match the table name.
+        
+        match = tbl_expr.search(schema, start_pos)
+        if match is None:
+            break
+
+        start_pos = schema.find(match.group(0)) + len(match.group(0))
+        table_list.append(match.group(1))
+
+    return table_list
 
 def create_database(schema):
+    """Create the glsr database."""
 
     db_match = re.search(_db_re, schema)
         
     if db_match is None:
-        raise SetupError("DB create not defined in schema" % table)
+        raise SetupError("DB create not defined in schema")
 
     print db_match.group(0)
 
@@ -136,10 +150,11 @@ def create_tables(tables, schema):
         #print str_indent(table_str.group(0), 4)
         #print ""
         
-        _db_obj.query(table_match.group(1), fetch="none")
+        _db_obj.query(table_match.group(0), fetch="none")
 
 def insert_data(table, schema):
-
+    """Insert additional SQL data."""
+    
     start = schema.find("# begin %s-data-insert" % table)
     start = start + len("# begin %s-data-insert" % table)
     end = schema.find("# end %s-data-insert" % table)
@@ -154,7 +169,7 @@ class SetupError(Exception): pass
 
 if __name__ == '__main__':
 
-    # Setup directories and files
+        # Setup directories and files
     print "Creating script repository directories and files:"
     print "  Creating log folder (%s)" % config.logging_loc
     if not os.path.exists(config.logging_loc):
@@ -164,32 +179,31 @@ if __name__ == '__main__':
     if not os.path.exists(config.template_cache):
         os.makedirs(config.template_cache)
 
-    user_loop = []
+    _user_loop = []
     admin_info = add_user("Admin")
     admin_info["rank"] = 0
     admin_info["type"] = 3
-    user_loop.append(admin_info)
+    _user_loop.append(admin_info)
 
     # Load the schema file
-    schema_loc = config.root + "/setup/schema/%s.schema" % config.db["db_type"]
-    schema = load_schema(schema_loc, {"USER_LOOP": user_loop})
+    _schema_loc = config.root + "setup/schema/%s.schema" % config.db["db_type"]
+    _schema = load_schema(_schema_loc, {"USER_LOOP": _user_loop})
 
     # Create the database and tables
     #print "Creating GLSR %s database" % config.db["db_type"]
     #create_database(schema)
 
     print "\nCreating %s tables" % config.db["db_type"]
-    create_tables(_list_tables(schema), schema)
+    create_tables(_list_tables(_schema), _schema)
 
     print "\nInserting default data:"
-    insert_data("state", schema)
+    insert_data("state", _schema)
 
     print "\nInserting language data:"
-    insert_data("language", schema)
+    insert_data("language", _schema)
 
     # Add the admin user
     print "\nCreating admin user %s..." % (admin_info["alias"])
-    insert_data("user", schema)
+    insert_data("user", _schema)
 
     print
-
