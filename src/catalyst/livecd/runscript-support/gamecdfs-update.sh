f4382f0837e346ee4ef0209a65d258c6ab80b5c1
Chris Gianelloni <wolf31o2@gentoo.org>
2005-04-09 15:52:20 +0000

    Updated to create a /var/db entry for xorg-x11, required for opengl-update.  Also make PORTAGE_TMPDIR=/tmp for opengl-update.

--- src/catalyst/livecd/runscript-support/gamecdfs-update.sh
+++ src/catalyst/livecd/runscript-support/gamecdfs-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/livecd/runscript-support/Attic/gamecdfs-update.sh,v 1.6.2.1 2005/04/06 11:41:38 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/livecd/runscript-support/Attic/gamecdfs-update.sh,v 1.6.2.2 2005/04/09 15:52:20 wolf31o2 Exp $
 
 # we grab our configuration
 source /tmp/gamecd.conf || exit 1
@@ -16,3 +16,10 @@ echo "exec ${GAME_EXECUTABLE}" > /etc/X11/xinit/xinitrc
 # we add spind to default here since we don't want the CD to spin down
 rc-update add spind default
 rc-update add x-setup default
+
+# This is my hack to reduce tmpfs usage
+mkdir -p /usr/livecd/db/pkg/x11-base
+mv /var/db/pkg/x11-base/xorg* /usr/livecd/db/pkg/x11-base
+
+# This should help setup our PORTAGE_TMPDIR
+echo 'PORTAGE_TMPDIR="/tmp"' >> /etc/make.conf
