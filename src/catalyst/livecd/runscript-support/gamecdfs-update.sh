a8d28e1cc55506a4e6fc07233e07b86dd4a7093e
Chris Gianelloni <wolf31o2@gentoo.org>
2005-04-14 00:32:14 +0000

    Fixing broken /var/db link and removing bad alsa module.

--- src/catalyst/livecd/runscript-support/gamecdfs-update.sh
+++ src/catalyst/livecd/runscript-support/gamecdfs-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/livecd/runscript-support/Attic/gamecdfs-update.sh,v 1.6.2.2 2005/04/09 15:52:20 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/livecd/runscript-support/Attic/gamecdfs-update.sh,v 1.6.2.3 2005/04/14 00:32:14 wolf31o2 Exp $
 
 # we grab our configuration
 source /tmp/gamecd.conf || exit 1
@@ -20,6 +20,11 @@ rc-update add x-setup default
 # This is my hack to reduce tmpfs usage
 mkdir -p /usr/livecd/db/pkg/x11-base
 mv /var/db/pkg/x11-base/xorg* /usr/livecd/db/pkg/x11-base
+rm -rf /var/db
 
 # This should help setup our PORTAGE_TMPDIR
 echo 'PORTAGE_TMPDIR="/tmp"' >> /etc/make.conf
+
+# This removes an error module from alsa
+rm -f /lib/modules/2.6.11-gentoo-r5/alsa-driver/pci/snd-hdspm.ko
+depmod -e 2.6.11-gentoo-r5
