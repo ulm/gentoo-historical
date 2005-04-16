94b55ca8d64768e5872cbad0fbe93c62573e5035
Andrew Gaffney <agaffney@gentoo.org>
2005-04-16 03:12:27 +0000

    documented GLINotification.py

--- src/installer/src/GLINotification.py
+++ src/installer/src/GLINotification.py
@@ -3,42 +3,39 @@ Gentoo Linux Installer
 
 Copyright 2004 Gentoo Technologies Inc.
 
-$Id: GLINotification.py,v 1.3 2005/04/14 15:44:03 agaffney Exp $
+$Id: GLINotification.py,v 1.4 2005/04/16 03:12:27 agaffney Exp $
 """
 
+##
+# This class provides a wrapper for passing data from the backend to the frontend
 class GLINotification:
 	##
-	# Brief description of function
-	# @param self Parameter description
-	# @param type Parameter description
-	# @param data Parameter description
+	# Initialization function for GLINotification
+	# @param type String specifying the type of data contained in the next parameter
+	# @param data Data to pass
 	def __init__(self, type, data):
 		self._type = type
 		self._data = data
 
 	##
-	# Brief description of function
-	# @param self Parameter description
+	# Retrieves data
 	def get_data(self):
 		return self._data
 
 	##
-	# Brief description of function
-	# @param self Parameter description
+	# Retrieves data type
 	def get_type(self):
 		return self._type
 
 	##
-	# Brief description of function
-	# @param self Parameter description
-	# @param data Parameter description
+	# Sets data
+	# @param data Data (duh)
 	def set_data(self, data):
 		self._data = data
 
 	##
-	# Brief description of function
-	# @param self Parameter description
-	# @param type Parameter description
+	# Sets data type
+	# @param type Type (duh)
 	def set_type(self, type):
 		self._type = type
 
