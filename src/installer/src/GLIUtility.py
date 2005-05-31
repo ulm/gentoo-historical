be75dabf04e23b8c2a5c856514ff24ce50232394
Andrew Gaffney <agaffney@gentoo.org>
2005-05-31 17:10:18 +0000

    Chroot wrapper   passes along exit code.

--- src/installer/src/GLIUtility.py
+++ src/installer/src/GLIUtility.py
@@ -288,7 +288,7 @@ def spawn(cmd, quiet=False, logfile=None, display_on_tty8=False, chroot=None, ap
 	# quiet and return_output really do the same thing. One of them need to be removed.
 	if chroot != None:
 		wrapper = open(chroot+"/tmp/spawn.sh", "w")
-		wrapper.write("#!/bin/bash\n\nsource /etc/profile\n" + cmd)
+		wrapper.write("#!/bin/bash\n\nsource /etc/profile\n" + cmd + "\nexit $?\n")
 		wrapper.close()
 #		print "running '" + cmd + "' in chroot " + chroot
 		cmd = "chmod a+x " + chroot + "/tmp/spawn.sh && chroot " + chroot + " /tmp/spawn.sh 2>&1"
