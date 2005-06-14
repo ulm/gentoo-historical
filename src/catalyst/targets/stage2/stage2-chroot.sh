6ec5e0f22a97bfb95d8f31485d695b15ca2e432c
Chris Gianelloni <wolf31o2@gentoo.org>
2005-06-14 19:13:38 +0000

    Added a -p bootstrap when catalyst is called with -V (verbose).

--- src/catalyst/targets/stage2/stage2-chroot.sh
+++ src/catalyst/targets/stage2/stage2-chroot.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage2/Attic/stage2-chroot.sh,v 1.10 2005/01/11 14:10:19 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage2/Attic/stage2-chroot.sh,v 1.10.2.1 2005/06/14 19:13:38 wolf31o2 Exp $
 
 /usr/sbin/env-update
 source /etc/profile
@@ -37,6 +37,18 @@ GRP_STAGE23_USE="$(portageq envvar GRP_STAGE23_USE)"
 
 ## setup the environment
 export FEATURES="${clst_myfeatures}"
+
+if [ "${clst_VERBOSE}" ]
+then
+	/usr/portage/scripts/bootstrap.sh -p ${bootstrap_opts}
+	echo "Press any key within 15 seconds to pause the build..."
+	read -s -t 15 -n 1
+	if [ $? -eq 0 ]
+	then
+		echo "Press any key to continue..."
+		read -s -n 1
+	fi
+fi
 
 ## START BUILD
 /usr/portage/scripts/bootstrap.sh ${bootstrap_opts} || exit 1
