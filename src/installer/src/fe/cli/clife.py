45f4b1bbb144ab54fe18a35781ea1b87d231898a
Robin H. Johnson <robbat2@gentoo.org>
2005-06-16 04:20:38 +0000

    Logging system.

--- src/installer/src/fe/cli/clife.py
+++ src/installer/src/fe/cli/clife.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/installer/src/fe/cli/clife.py,v 1.2 2005/06/14 05:17:28 robbat2 Exp $
+# $Header: /var/cvsroot/gentoo/src/installer/src/fe/cli/clife.py,v 1.3 2005/06/16 04:20:38 robbat2 Exp $
 
 import sys
 sys.path.append("../..")
@@ -9,10 +9,20 @@ sys.path.append("../..")
 import GLIInstallProfile
 import GLIClientConfiguration
 import GLIClientController
+from GLILogger import Logger
 import time
 from optparse import OptionParser
 
+clilog = None
+
+def msg(s):
+	print s
+	if clilog != None:
+		clilog.log(s)
+	
+
 def main():
+	clilog = Logger("/var/log/installer-cli.log")
 	usage = "usage: %prog [options] CLIENT.xml INSTALL.xml"
 	parser = OptionParser(usage)
 	parser.add_option("-p", "--pretend", dest="pretend_install",action="store_true",help="Pretend",default=False)
@@ -29,19 +39,19 @@ def main():
 	install_profile = GLIInstallProfile.InstallProfile()
 	cc = GLIClientController.GLIClientController(pretend=options.pretend_install)
 	
-	print "Loading client profile: "+client_config_xml_file
+	msg("Loading client profile: "+client_config_xml_file)
 	client_profile.parse(client_config_xml_file)
 	client_profile.set_interactive(None, False, None)
 	cc.set_configuration(client_profile)
 	cc.start_pre_install()
 	
-	print "Loading install profile: "+install_profile_xml_file
+	msg("Loading install profile: "+install_profile_xml_file)
 	install_profile.parse(install_profile_xml_file)
 	cc.set_install_profile(install_profile)
 	cc.start_install()
 	if options.pretend_install:
-		print "Pretending ",
-	print "Installation Started!"
+		msg("Pretending ")
+	msg("Installation Started!")
 	num_steps_completed = 1
 	while 1:
 		notification = cc.getNotification()
@@ -51,22 +61,21 @@ def main():
 		notify_type = notification.get_type()
 		notify_data = notification.get_data()
 		if notify_type == "exception":
-			print "Exception ("+repr(notify_data)+") received:"
-			print notify_data
+			msg("Exception ("+repr(notify_data)+") received:\n"+str(notify_data))
 		elif notify_type == "int":
 			if notify_data == GLIClientController.NEXT_STEP_READY:
 				next_step = cc.get_next_step_info()
 				num_steps = cc.get_num_steps()
 				i = (num_steps_completed*100)/num_steps
 				s = "On step %d of %d. Current step: %s" % (num_steps_completed, num_steps, next_step)
-				print s
+				msg(s)
 				num_steps_completed += 1
 				if cc.has_more_steps():
 					cc.next_step()
 				continue
 			if notify_data == GLIClientController.INSTALL_DONE:
-				print "Install completed"
-				print "Install done!"
+				msg("Install completed")
+				msg("Install done!")
 				sys.exit(0)
 
 if __name__ == "__main__":
