de3b80a6384b44ead11f4da747b222d456cade89
Chris Gianelloni <wolf31o2@gentoo.org>
2005-06-30 21:36:28 +0000

    Disabled ccache support for stage2, since it won't work properly anyway.

--- src/catalyst/targets/stage2/stage2-chroot.sh
+++ src/catalyst/targets/stage2/stage2-chroot.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage2/Attic/stage2-chroot.sh,v 1.10.2.1 2005/06/14 19:13:38 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage2/Attic/stage2-chroot.sh,v 1.10.2.2 2005/06/30 21:36:28 wolf31o2 Exp $
 
 /usr/sbin/env-update
 source /etc/profile
 
 [ -f /tmp/envscript ] && source /tmp/envscript
 
-if [ -n "${clst_CCACHE}" ]
-then
-	export clst_myfeatures="${clst_myfeatures} ccache"
-	emerge -b -k --oneshot --nodeps ccache || exit 1
-fi
+#if [ -n "${clst_CCACHE}" ]
+#then
+#	export clst_myfeatures="${clst_myfeatures} ccache"
+#	emerge -b -k --oneshot --nodeps ccache || exit 1
+#fi
 
 if [ -n "${clst_DISTCC}" ]
 then
