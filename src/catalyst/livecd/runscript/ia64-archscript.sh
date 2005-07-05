1a51dff8b7115e31198694bda64acd73bf0dd4c5
Chris Gianelloni <wolf31o2@gentoo.org>
2005-07-05 17:52:32 +0000

    Adding initial (and broken) ia64-archscript.sh to be fixed later.

---
+++ src/catalyst/livecd/runscript/ia64-archscript.sh
@@ -0,0 +1,117 @@
+# Copyright 1999-2004 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /var/cvsroot/gentoo/src/catalyst/livecd/runscript/Attic/ia64-archscript.sh,v 1.1.2.1 2005/07/05 17:52:32 wolf31o2 Exp $
+
+case $1 in
+	kernel)
+	;;
+
+	preclean)
+	;;
+
+	clean)
+	;;
+
+	bootloader)
+		# Create a filesystem tree for the ISO at
+		# ${clst_cdroot_path}. We extract the "cdtar" to this directory,
+		# which will normally contains a pre-built binary
+		# boot-loader/filesystem skeleton for the ISO. 
+		
+		cdtar=${clst_livecd_cdtar}
+		[ -z "$cdtar" ] && die "Required key livecd/cdtar not specified, exiting"
+		/bin/tar xjpvf ${cdtar} -C ${clst_cdroot_path} || die "Couldn't extract cdtar ${cdtar}"
+		[ -z "${clst_boot_kernel}" ] && die "Required key boot/kernel not specified, exiting"
+		
+		# unpack the kernel(s) that were built in kmerge.sh
+		first=""
+		for x in ${clst_boot_kernel}
+		do
+			kbinary="${clst_chroot_path}/usr/portage/packages/gk_binaries/${x}-${clst_version_stamp}.tar.bz2"
+			[ ! -e "${kbinary}" ] && die "Can't find kernel tarball at ${kbinary}"
+			
+			/bin/tar xjvf ${kbinary} -C ${clst_cdroot_path}/boot
+			
+			# change kernel name from "kernel" to "gentoo", for example
+			mv ${clst_cdroot_path}/boot/kernel* ${clst_cdroot_path}/boot/${x}
+			
+			# change initrd name from "initrd" to "gentoo.igz", for example
+			if [ -e ${clst_cdroot_path}/boot/initrd* ]
+			then
+			    mv "${clst_cdroot_path}"/boot/initrd* "${clst_cdroot_path}"/boot/"${x}".igz
+			fi
+			if [ -e ${clst_cdroot_path}/boot/initramfs* ]
+			then
+			    mv "${clst_cdroot_path}"/boot/initramfs* "${clst_cdroot_path}"/boot/"${x}".igz
+			fi
+			
+		done
+
+		# figure out what device manager we are using and handle it accordingly
+		if [ "${clst_livecd_devmanager}" == "udev" ]
+		then
+			cmdline_opts="${cmdline_opts} udev nodevfs"
+		elif [ "${clst_livecd_devmanager}" == "devfs" ]
+		then
+			cmdline_opts="${cmdline_opts} noudev devfs"
+		fi
+
+		# Add any additional options
+		if [ -n "${clst_livecd_bootargs}" ]
+		then
+			for x in ${clst_livecd_bootargs}
+			do
+				cmdline_opts="${cmdline_opts} ${x}"
+			done
+		fi
+
+		iacfg=${clst_cdroot_path}/boot/elilo.conf
+		echo 'prompt' > ${iacfg}
+		echo 'message=/efi/boot/elilo.msg' >> ${iacfg}
+		echo 'chooser=simple' >> ${iacfg}
+		echo 'timeout=50' >> ${iacfg}
+		echo >> ${iacfg}
+		for x in ${clst_boot_kernel}
+		do
+			echo "image=/efi/boot/${x}" >> ${iacfg}
+			echo "  label=${x}" >> ${iacfg}
+			echo '  append="initrd=${x}.igz root=/dev/ram0 init=/linuxrc acpi=off ${cmdline_opts} ${custom_kopts} cdroot"' >> ${iacfg}
+			echo "  initrd=/efi/boot/${x}.igz" >> ${iacfg}
+			echo >> ${iacfg}
+			echo "image=/efi/boot/${x}" >> ${iacfg}
+			echo "  label=${x}-serial">> ${iacfg}
+			echo "  append='initrd=${x}.igz root=/dev/ram0 init=/linuxrc acpi=off ${cmdline_opts} ${custom_kopts} cdroot console=tty0 console=ttyS0,9600'" >> ${iacfg}
+			echo "  initrd=/efi/boot/${x}.igz" >> ${iacfg}
+			echo >> ${iacfg}
+		done
+	;;
+
+	cdfs)
+	;;
+
+	iso)
+		# Set sensible default volume ID
+		if [ -z "${iso_volume_id}" ]
+		then
+			case ${clst_livecd_type} in
+				gentoo-*)
+					iso_volume_id="Gentoo Linux - Alpha"
+				;;
+				*)
+					iso_volume_id="Catalyst LiveCD"
+				;;
+			esac
+		fi
+		# this is for the livecd-final target, and calls the proper
+		# command to build the iso file
+		case ${clst_livecd_cdfstype} in
+			zisofs)
+				mkisofs -J -R -l -z -V "${iso_volume_id}" -o ${2} ${clst_cdroot_path}  || die "Cannot make ISO image"
+			;;
+			*)
+				mkisofs -J -R -l -V "${iso_volume_id}" -o ${2} ${clst_cdroot_path} || die "Cannot make ISO image"
+			;;
+		esac
+		isomarkboot ${2} /boot/bootlx
+	;;
+esac
