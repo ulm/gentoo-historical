50e95ae781a554b5bf4f921adf9bf2ab078868fe
Chris Gianelloni <wolf31o2@gentoo.org>
2005-07-14 20:19:26 +0000

    Added removal of /var/log/emerge.log to stage cleanup.

--- src/catalyst/targets/stage1/stage1-preclean2-chroot.sh
+++ src/catalyst/targets/stage1/stage1-preclean2-chroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage1/Attic/stage1-preclean2-chroot.sh,v 1.10.2.3 2005/07/05 21:47:46 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage1/Attic/stage1-preclean2-chroot.sh,v 1.10.2.4 2005/07/14 20:19:26 wolf31o2 Exp $
 		
 # now, some finishing touches to initialize gcc-config....
 unset ROOT
@@ -37,5 +37,4 @@ else
 	echo UTC > /etc/TZ
 fi
 
-# this cleans out /var/db, but leaves behind files portage needs for removal
-#find /var/db/pkg -type f | grep -v '\(COUNTER\|CONTENTS\|SLOT\|ebuild\)' | xargs rm -f
+rm -f /var/log/emerge.log
--- src/catalyst/targets/stage2/stage2-preclean-chroot.sh
+++ src/catalyst/targets/stage2/stage2-preclean-chroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage2/Attic/stage2-preclean-chroot.sh,v 1.4.2.1 2005/07/05 21:47:46 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage2/Attic/stage2-preclean-chroot.sh,v 1.4.2.2 2005/07/14 20:19:26 wolf31o2 Exp $
 
 /usr/sbin/env-update
 source /etc/profile
@@ -17,3 +17,5 @@ if [ -n "${clst_DISTCC}" ]
 then
 	emerge -C sys-devel/distcc || exit 1
 fi
+
+rm -f /var/log/emerge.log
