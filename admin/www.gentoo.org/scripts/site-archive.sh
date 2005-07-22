ac01d210b0a9c294ef4bec6178aa122495245024
cvs-fast-export <cvs-fast-export>
2005-07-22 19:38:00 +0000

    Synthetic commit for incomplete tag patches-2_3_5-1_6

--- admin/www.gentoo.org/scripts/site-archive.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-[ -z "${GENTOO_WEB_DOCROOT}" ] && echo "\$WEBROOT not set; exiting" && exit 1
-install -d ${GENTOO_WEB_DOCROOT}/dyn/arch
-cd ${GENTOO_WEB_DOCROOT}/..
-tar cvzf ${GENTOO_WEB_DOCROOT}/dyn/arch/xml-guide-latest.tar.gz --exclude htdocs/dyn htdocs
-
