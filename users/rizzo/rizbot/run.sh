a86945eb42c525a2cfeac0176e41ce7f470cb89e
cvs-fast-export <cvs-fast-export>
2005-07-28 16:15:04 +0000

    Synthetic commit for incomplete tag GRUB-0-96-R2

--- users/rizzo/rizbot/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -Djava.awt.headless=true -classpath ./lib/rizbot.jar:./lib/pircbot.jar:./lib/PieSpy.jar:./lib/JMegaHal.jar:./lib/Reminder.jar org.gentoo.rizzo.rizbot.RizBot ./config.ini
