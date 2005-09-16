140e31527c904af4daabc60752b03b05c795169f
cvs-fast-export <cvs-fast-export>
2005-09-16 17:44:23 +0000

    Synthetic commit for incomplete tag compiler-config-2_0_alpha1

--- src/catalyst/targets/tinderbox/tinderbox-preclean-chroot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Copyright 1999-2005 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/tinderbox/Attic/tinderbox-preclean-chroot.sh,v 1.6 2005/07/05 21:53:41 wolf31o2 Exp $
-
-. /tmp/chroot-functions.sh
-update_env_settings
