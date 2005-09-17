e47b1c197669086285f03f73ca564cdc0e70533a
Andrew Gaffney <agaffney@gentoo.org>
2005-09-17 03:45:35 +0000

    add rot13ify() function

--- src/installer/src/GLIUtility.py
+++ src/installer/src/GLIUtility.py
@@ -861,3 +861,16 @@ def get_grp_pkgs_from_cd():
 	for pkg in output:
 		results.append(pkg[(pkg.find('/')+1):])
 	return results
+
+def rot13ify(text):
+	newtext = ""
+	for x in range(len(text)):
+		byte = ord(text[x])
+		cap = (byte & 32)
+		byte = (byte & (~cap))
+		if(byte >= ord('A')) and (byte <= ord('Z')):
+			byte = ((byte - ord('A') + 13) % 26 + ord('A'))
+		byte = (byte | cap)
+		newtext += chr(byte)
+	return newtext
+
