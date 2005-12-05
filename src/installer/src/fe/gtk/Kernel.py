1cb27d63af68b339c93e5baf93d7f66a9099d369
Andrew Gaffney <agaffney@gentoo.org>
2005-12-05 01:09:41 +0000

    bootsplash option on Kernel screen

--- src/installer/src/fe/gtk/Kernel.py
+++ src/installer/src/fe/gtk/Kernel.py
@@ -25,10 +25,10 @@ a brief description beside it.
 
 		self.kernel_sources['gentoo-sources'] = gtk.RadioButton(None, "gentoo-sources")
 		self.kernel_sources['gentoo-sources'].set_name("gentoo-sources")
-		self.kernel_sources['gentoo-sources'].connect("toggled", self.stage_selected, "gentoo-sources")
+		self.kernel_sources['gentoo-sources'].connect("toggled", self.kernel_selected, "gentoo-sources")
 		self.kernel_sources['gentoo-sources'].set_size_request(150, -1)
 		hbox = gtk.HBox(False, 0)
-		hbox.pack_start(self.kernel_sources['gentoo-sources'], expand=False, fill=False, padding=5)
+		hbox.pack_start(self.kernel_sources['gentoo-sources'], expand=False, fill=False, padding=0)
 		tmplabel = gtk.Label("These are the vanilla sources patched with the Gentoo patchset. These are generally considered stable.")
 		tmplabel.set_line_wrap(True)
 		hbox.pack_start(tmplabel, expand=False, fill=False, padding=20)
@@ -36,10 +36,10 @@ a brief description beside it.
 
 		self.kernel_sources['vanilla-sources'] = gtk.RadioButton(self.kernel_sources['gentoo-sources'], "vanilla-sources")
 		self.kernel_sources['vanilla-sources'].set_name("vanilla-sources")
-		self.kernel_sources['vanilla-sources'].connect("toggled", self.stage_selected, "vanilla-sources")
+		self.kernel_sources['vanilla-sources'].connect("toggled", self.kernel_selected, "vanilla-sources")
 		self.kernel_sources['vanilla-sources'].set_size_request(150, -1)
 		hbox = gtk.HBox(False, 0)
-		hbox.pack_start(self.kernel_sources['vanilla-sources'], expand=False, fill=False, padding=5)
+		hbox.pack_start(self.kernel_sources['vanilla-sources'], expand=False, fill=False, padding=0)
 		tmplabel = gtk.Label("These are the kernel sources straight from kernel.org without patches (except where necessary)")
 		tmplabel.set_line_wrap(True)
 		hbox.pack_start(tmplabel, expand=False, fill=False, padding=20)
@@ -47,18 +47,27 @@ a brief description beside it.
 
 		self.kernel_sources['livecd-kernel'] = gtk.RadioButton(self.kernel_sources['gentoo-sources'], "livecd-kernel")
 		self.kernel_sources['livecd-kernel'].set_name("livecd-kernel")
-		self.kernel_sources['livecd-kernel'].connect("toggled", self.stage_selected, "livecd-kernel")
+		self.kernel_sources['livecd-kernel'].connect("toggled", self.kernel_selected, "livecd-kernel")
 		self.kernel_sources['livecd-kernel'].set_size_request(150, -1)
 		hbox = gtk.HBox(False, 0)
-		hbox.pack_start(self.kernel_sources['livecd-kernel'], expand=False, fill=False, padding=5)
+		hbox.pack_start(self.kernel_sources['livecd-kernel'], expand=False, fill=False, padding=0)
 		tmplabel = gtk.Label("This will install the LiveCD's kernel/initrd into your new system. Use this option to get your system up and running quickly. You should not tell the installer to emerge any packages that require kernel sources as they won't be present.")
+		tmplabel.set_line_wrap(True)
+		hbox.pack_start(tmplabel, expand=False, fill=False, padding=20)
+		vert.pack_start(hbox, expand=False, fill=False, padding=10)
+
+		hbox = gtk.HBox(False, 0)
+		self.bootsplash_check = gtk.CheckButton("Enable bootsplash")
+		self.bootsplash_check.set_size_request(150, -1)
+		hbox.pack_start(self.bootsplash_check, expand=False, fill=False, padding=0)
+		tmplabel = gtk.Label("This enables a colorful background image during system boot.")
 		tmplabel.set_line_wrap(True)
 		hbox.pack_start(tmplabel, expand=False, fill=False, padding=20)
 		vert.pack_start(hbox, expand=False, fill=False, padding=10)
 
 		self.add_content(vert)
 
-	def stage_selected(self, widget, data=None):
+	def kernel_selected(self, widget, data=None):
 		self.active_selection = data
 
 	def activate(self):
@@ -69,9 +78,11 @@ a brief description beside it.
 		self.controller.SHOW_BUTTON_FINISH  = False
 		self.active_selection = self.controller.install_profile.get_kernel_source_pkg() or "gentoo-sources"
 		self.kernel_sources[self.active_selection].set_active(True)
+		self.bootsplash_check.set_active(self.controller.install_profile.get_kernel_bootsplash())
 
 	def deactivate(self):
 		self.controller.install_profile.set_kernel_source_pkg(None, self.active_selection, None)
 		# For now
 		self.controller.install_profile.set_kernel_build_method(None, "genkernel", None)
+		self.controller.install_profile.set_kernel_bootsplash(None, self.bootsplash_check.get_active(), None)
 		return True
