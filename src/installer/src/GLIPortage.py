a0991716c6217c2e5aaa0e4deab603531d97c565
Andrew Gaffney <agaffney@gentoo.org>
2006-01-03 04:09:36 +0000

    remove link if it exists before moving content into place

--- src/installer/src/GLIPortage.py
+++ src/installer/src/GLIPortage.py
@@ -5,10 +5,11 @@
 # of which can be found in the main directory of this project.
 Gentoo Linux Installer
 
-$Id: GLIPortage.py,v 1.27 2006/01/03 02:31:38 agaffney Exp $
+$Id: GLIPortage.py,v 1.28 2006/01/03 04:09:36 agaffney Exp $
 """
 
 import re
+import os
 import GLIUtility
 from GLIException import GLIException
 
@@ -57,7 +58,6 @@ class GLIPortage(object):
 
 		tmpdir = "/var/tmp/portage"
 		image_dir = tmpdir + "/" + package.split("/")[1] + "/image"
-
 		root_cmd = ""
 		tmp_chroot_dir = self._chroot_dir
 		portage_tmpdir = "/var/tmp/portage"
@@ -87,9 +87,9 @@ class GLIPortage(object):
 				for entry in entries:
 					parts = entry.split(" ")
 					# Hack for symlink crappiness
-					for symlink in symlinks:
-						if parts[0].startswith(symlink):
-							parts[0] = symlinks[symlink] + parts[0][len(symlink):]
+#					for symlink in symlinks:
+#						if parts[0].startswith(symlink):
+#							parts[0] = symlinks[symlink] + parts[0][len(symlink):]
 					tarfiles.write(parts[0] + "\n")
 				tarfiles.close()
 			except:
@@ -104,8 +104,11 @@ class GLIPortage(object):
 			for symlink in symlinks:
 				if GLIUtility.is_file(self._chroot_dir + image_dir + symlinks[symlink]):
 					if self._debug: self._logger.log("DEBUG: copy_pkg_to_chroot(): fixing " + symlink + " symlink ickiness stuff in " + image_dir + " for " + package)
+					if os.path.islink(self._chroot_dir + image_dir + symlink):
+						if not GLIUtiltiy.exitsuccess(GLIUtility.spawn("rm " + self._chroot_dir + image_dir + symlink)):
+							raise GLIException("CopyPackageToChrootError", 'fatal', 'copy_pkg_to_chroot', "Could not remove symlink " + symlink + " for " + package)
 					if not GLIUtility.exitsuccess(GLIUtility.spawn("mv " + self._chroot_dir + image_dir + symlinks[symlink] + " " + self._chroot_dir + image_dir + symlink)):
-						raise GLIException("CopyPackageToChrootError", 'fatal', 'copy_pkg_to_chroot', "Could not fix /usr/livecd/gconf/ stuff for " + package)
+						raise GLIException("CopyPackageToChrootError", 'fatal', 'copy_pkg_to_chroot', "Could not fix " + symlink + " symlink ickiness for " + package)
 
 		# Run pkg_setup
 		if self._debug: self._logger.log("DEBUG: copy_pkg_to_chroot(): running pkg_setup for " + package)
