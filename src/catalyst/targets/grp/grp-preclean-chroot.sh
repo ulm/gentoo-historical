ffdbfba435cb40a8bb5f3e2b65dae0fdddb9eeea
cvs-fast-export <cvs-fast-export>
2006-01-12 00:35:15 +0000

    Synthetic commit for incomplete tag vlc-patches-13

--- src/catalyst/targets/grp/grp-preclean-chroot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright 1999-2005 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/grp/Attic/grp-preclean-chroot.sh,v 1.9 2005/12/16 19:14:46 wolf31o2 Exp $
-
-. /tmp/chroot-functions.sh
-update_env_settings
-
-gconftool-2 --shutdown
--- src/catalyst/targets/livecd-stage1/livecd-stage1-preclean-chroot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Copyright 1999-2005 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/livecd-stage1/Attic/livecd-stage1-preclean-chroot.sh,v 1.7 2005/04/04 17:48:33 rocket Exp $
-
-. /tmp/chroot-functions.sh
-update_env_settings
