943589c2236550d711f450fb2a9820bc31088d37
Andrew Gaffney <agaffney@gentoo.org>
2006-02-05 20:45:01 +0000

    added Othersettings to gtkfe

--- src/installer/src/fe/gtk/gtkfe.py
+++ src/installer/src/fe/gtk/gtkfe.py
@@ -45,7 +45,7 @@ class Installer:
                   { 'text': _('Daemons'), 'module': __import__("Daemons") },
                   { 'text': _('Extra Packages'), 'module': __import__("ExtraPackages") },
                   { 'text': _('Startup Services'), 'module': __import__("StartupServices") },
-                  { 'text': _('Rc.conf'), 'module': __import__("RcDotConf") },
+                  { 'text': _('Other Settings'), 'module': __import__("OtherSettings") },
                   { 'text': _('Users'), 'module': __import__("Users") },
                   { 'text': _('Review'), 'module': __import__("InstallSummary") }
                 ]
