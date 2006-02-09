30080256e0acef99080f044e47cf2c6d6d621fee
Andrew Gaffney <agaffney@gentoo.org>
2006-02-09 23:49:01 +0000

    make symlink without /

--- src/installer/src/GLIArchitectureTemplate.py
+++ src/installer/src/GLIArchitectureTemplate.py
@@ -5,7 +5,7 @@
 # of which can be found in the main directory of this project.
 Gentoo Linux Installer
 
-$Id: GLIArchitectureTemplate.py,v 1.260 2006/02/09 23:45:50 agaffney Exp $
+$Id: GLIArchitectureTemplate.py,v 1.261 2006/02/09 23:49:01 agaffney Exp $
 
 The ArchitectureTemplate is largely meant to be an abstract class and an 
 interface (yes, it is both at the same time!). The purpose of this is to create 
@@ -258,7 +258,7 @@ class ArchitectureTemplate:
 			# Pre-create /lib (and possible /lib32 and /lib64)
 			if os.path.islink("/lib") and os.readlink("/lib") == "lib64":
 				if self._debug: self._logger.log("DEBUG: unpack_stage_tarball(): precreating /lib64 dir and /lib -> /lib64 symlink because glibc/portage sucks")
-				if not GLIUtility.exitsuccess(GLIUtility.spawn("mkdir " + self._chroot_dir + "/lib64 && ln -s /lib64 " + self._chroot_dir + "/lib")):
+				if not GLIUtility.exitsuccess(GLIUtility.spawn("mkdir " + self._chroot_dir + "/lib64 && ln -s lib64 " + self._chroot_dir + "/lib")):
 					raise GLIException("CreateStage3Error", "fatal", "unpack_stage_tarball", "Could not precreate /lib64 dir and /lib -> /lib64 symlink")
 
 			syspkglen = len(systempkgs)
