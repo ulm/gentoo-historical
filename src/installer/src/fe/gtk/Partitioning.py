212979ae9b641922e1f3f7da296b5c02acc4ab7c
Andrew Gaffney <agaffney@gentoo.org>
2006-03-10 02:12:37 +0000

    pink for HFS

--- src/installer/src/fe/gtk/Partitioning.py
+++ src/installer/src/fe/gtk/Partitioning.py
@@ -27,8 +27,8 @@ class Panel(GLIScreen.GLIScreen):
 	active_part_cur_size = 0
 	active_part_start_cyl = 0
 	active_part_minor = 0
-	colors = { 'ext2': '#0af2fe', 'ext3': '#0af2fe', 'unalloc': '#a2a2a2', 'unknown': '#ed03e0', 'free': '#ffffff', 'ntfs': '#f20600', 'fat16': '#3d07f9', 'fat32': '#3d07f9', 'reiserfs': '#f0ff00', 'linux-swap': '#12ff09', 'xfs': '#006600', 'jfs': '#ffb400' }
-	supported_filesystems = ["ext2", "ext3", "linux-swap", "xfs", "jfs", "reiserfs", "fat16", "fat32", "ntfs"]
+	colors = { 'ext2': '#0af2fe', 'ext3': '#0af2fe', 'unalloc': '#a2a2a2', 'unknown': '#ed03e0', 'free': '#ffffff', 'ntfs': '#f20600', 'fat16': '#3d07f9', 'fat32': '#3d07f9', 'reiserfs': '#f0ff00', 'linux-swap': '#12ff09', 'xfs': '#006600', 'jfs': '#ffb400', 'hfs': '#fd80ff' }
+	supported_filesystems = ["ext2", "ext3", "linux-swap", "xfs", "jfs", "reiserfs", "fat16", "fat32", "ntfs", "hfs"]
 	_helptext = """
 <b><u>Partitioning</u></b>
 
@@ -186,6 +186,7 @@ your existing partitions will very likely be destroyed.
                                 { 'label': "XFS", 'color': '#006600' },
                                 { 'label': "FAT", 'color': '#3d07f9' },
                                 { 'label': "NTFS", 'color': '#f20600' },
+                                { 'label': "HFS", 'color': '#fd80ff' },
                                 { 'label': _("Other"), 'color': '#ed03e0' },
 #                                { 'label': "Free space", 'color': '#ffffff' },
                                 { 'label': _("Unallocated"), 'color': '#a2a2a2' }
