f9f9d1539db6ebb36efddbd520ee42212e745a61
Andrew Gaffney <agaffney@gentoo.org>
2006-03-13 23:59:35 +0000

    added parttest2.py

---
+++ src/installer/src/tests/parttest2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append("..")
+import GLIStorageDevice
+
+if len(sys.argv) <= 1:
+	print "You must call this script with a device name"
+	sys.exit(1)
+
+device = GLIStorageDevice.Device(sys.argv[1])
+print "Device: " + sys.argv[1]
+print "Disklabel: " + device.get_disklabel()
+print "Length (MB): " + str(device.get_total_mb())
+print "Length (sectors): " + str(device.get_num_sectors())
+for partition in device.get_partitions():
+	print "--------------------"
+	print "Minor: " + str(partition.get_minor())
+	print "Type: " + str(partition.get_type())
+	try:
+		print "Start: " + str(partition.get_start())
+		print "End: " + str(partition.get_end())
+	except:
+		pass
