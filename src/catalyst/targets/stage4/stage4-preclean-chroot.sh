97116abab5c9a8d5f977ebfe508833d80f4f91ad
cvs-fast-export <cvs-fast-export>
2006-06-21 02:57:57 +0000

    Synthetic commit for incomplete tag eselect-compiler-2_0_0_rc2

--- src/catalyst/targets/stage4/stage4-preclean-chroot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Copyright 1999-2005 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage4/Attic/stage4-preclean-chroot.sh,v 1.3 2005/12/19 15:36:02 wolf31o2 Exp $
-
-. /tmp/chroot-functions.sh
-#update_env_settings
