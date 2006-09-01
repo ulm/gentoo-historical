b381b0500d1eb66ae665e958200911eeea346c47
Mike Frysinger <vapier@gentoo.org>
2006-09-01 07:38:06 +0000

    sync with latest baselayout

--- src/netboot-base/src/consoletype.c
+++ src/netboot-base/src/consoletype.c
@@ -3,16 +3,16 @@
  * simple app to figure out whether the current terminal
  * is serial, console (vt), or remote (pty).
  *
- * Copyright 1999-2004 Gentoo Foundation
+ * Copyright 1999-2006 Gentoo Foundation
  * Distributed under the terms of the GNU General Public License v2
- * $Header: /var/cvsroot/gentoo/src/netboot-base/src/consoletype.c,v 1.1 2004/10/07 20:38:15 vapier Exp $
+ * $Header: /var/cvsroot/gentoo/src/netboot-base/src/consoletype.c,v 1.2 2006/09/01 07:38:06 vapier Exp $
  */
 
 #include <stdio.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
-#include <sys/sysmacros.h>
+#include "headers.h"
 
 int main(int argc, char *argv[])
 {
@@ -23,13 +23,14 @@ int main(int argc, char *argv[])
 	fstat(0, &sb);
 	maj = major(sb.st_rdev);
 	if (maj != 3 && (maj < 136 || maj > 143)) {
+#if defined(__linux__)
 		if (ioctl (0, TIOCLINUX, &twelve) < 0) {
 			printf("serial\n");
 			return 1;
-		} else {
-			printf("vt\n");
-			return 0;
 		}
+#endif
+		printf("vt\n");
+		return 0;
 	} else {
 		printf("pty\n");
 		return 2;
