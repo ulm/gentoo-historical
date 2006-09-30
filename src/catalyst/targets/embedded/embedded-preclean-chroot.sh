9517c5c59ce6451740b175cb64b381e8d7570945
cvs-fast-export <cvs-fast-export>
2006-09-30 18:22:06 +0000

    Synthetic commit for incomplete tag v1_3_1

--- src/catalyst/targets/embedded/embedded-preclean-chroot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright 1999-2005 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/embedded/Attic/embedded-preclean-chroot.sh,v 1.8 2006/01/17 19:30:45 wolf31o2 Exp $
-
-. /tmp/chroot-functions.sh
-update_env_settings
-cleanup_distcc
