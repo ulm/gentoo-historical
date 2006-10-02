c527a583ad2b2c0aa7b20fc077c7c4616958f4a0
cvs-fast-export <cvs-fast-export>
2006-10-02 03:42:08 +0000

    Synthetic commit for incomplete tag libao-patches-3

--- src/catalyst/targets/embedded/unmerge.sh
+++
@@ -1,7 +0,0 @@
-# Copyright 1999-2005 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/embedded/Attic/unmerge.sh,v 1.3 2005/07/05 21:53:41 wolf31o2 Exp $
-
-${clst_CHROOT} ${clst_chroot_path} /bin/bash << EOF
-	ROOT=/tmp/mergeroot emerge -C $* || exit 1
-EOF
