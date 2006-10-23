68e0d588f1234f70139b0d7319c566796ad8c4d4
Stefan Schweizer <genstef@gentoo.org>
2006-10-23 18:56:03 +0000

    improve scripts to get langpacks for firefox2

--- src/patchsets/mozilla-firefox/getpacks.sh
+++ src/patchsets/mozilla-firefox/getpacks.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
-# $Header: /var/cvsroot/gentoo/src/patchsets/mozilla-firefox/getpacks.sh,v 1.4 2006/09/15 02:39:32 redhatter Exp ${app}/make-tarball.sh,v 1.2 2006/07/30 10:44:28 redhatter Exp $
+# $Header: /var/cvsroot/gentoo/src/patchsets/mozilla-firefox/getpacks.sh,v 1.5 2006/10/23 18:56:03 genstef Exp ${app}/make-tarball.sh,v 1.2 2006/07/30 10:44:28 redhatter Exp $
 
-app=firefox
+PN=mozilla-firefox
 
 if [[ $# -ne 1 ]] ; then
-	echo "Usage: $0 <${app} version>"
+	echo "Usage: $0 <${PN} portage version>"
 	exit 1
 fi
-ver=$1
+PV=$1
+MY_PV=${PV/_}
+P=${PN}-${PV}
+S=${P}-xpi
 
-mkdir "langpacks-${ver}"
-wget -P "langpacks-${ver}" -m -np -nd \
-	"ftp://releases.mozilla.org/pub/mozilla.org/firefox/releases/${ver}/linux-i686/xpi/"
+mkdir ${S}
+wget -P "${S}" -m -np -nd \
+	"ftp://releases.mozilla.org/pub/mozilla.org/firefox/releases/${MY_PV}/linux-i686/xpi/"
 
-cd "langpacks-${ver}"
+cd ${S}
 for f in *.xpi; do
 	bn="$( basename "${f}" .xpi)"
 	locales="${locales} ${bn}"
-	mv -v "${f}" "${app}-${bn}-${ver}.xpi"
+	mv -v "${f}" "${P}-${bn}.xpi"
 done
 cd "${OLDPWD}"
 echo "Locales: ${locales}"
