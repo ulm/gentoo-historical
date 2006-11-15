bf964ef9d2efcebb1602ee5dbf7d0429133e4525
cvs-fast-export <cvs-fast-export>
2006-11-15 21:03:56 +0000

    Synthetic commit for incomplete tag vlc-patches-28

--- src/catalyst/targets/livecd-stage1/livecd-stage1-preclean-chroot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/livecd-stage1/Attic/livecd-stage1-preclean-chroot.sh,v 1.9 2006/10/02 20:41:54 wolf31o2 Exp $
-
-. /tmp/chroot-functions.sh
-update_env_settings
-cleanup_distcc
