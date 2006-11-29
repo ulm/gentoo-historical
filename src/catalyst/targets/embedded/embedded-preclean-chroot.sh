58bd08ef394af40cdb19e3df42f6e1cb48039360
cvs-fast-export <cvs-fast-export>
2006-11-29 22:48:23 +0000

    Synthetic commit for incomplete tag xine-lib-patches-64

--- src/catalyst/targets/embedded/embedded-preclean-chroot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/embedded/Attic/embedded-preclean-chroot.sh,v 1.10 2006/10/02 20:41:54 wolf31o2 Exp $
-
-. /tmp/chroot-functions.sh
-update_env_settings
-
-cleanup_distcc
--- src/catalyst/targets/grp/grp-preclean-chroot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/grp/Attic/grp-preclean-chroot.sh,v 1.11 2006/10/02 20:41:54 wolf31o2 Exp $
-
-. /tmp/chroot-functions.sh
-update_env_settings
-cleanup_distcc
-
-gconftool-2 --shutdown
