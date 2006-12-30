1bfb72ad50ce062d495a6e31cb7c08e3f822fa83
cvs-fast-export <cvs-fast-export>
2006-12-30 18:36:02 +0000

    Synthetic commit for incomplete tag vlc-patches-32

--- src/catalyst/targets/tinderbox/tinderbox-preclean-chroot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/tinderbox/Attic/tinderbox-preclean-chroot.sh,v 1.9 2006/10/02 20:41:54 wolf31o2 Exp $
-
-. /tmp/chroot-functions.sh
-update_env_settings
-cleanup_distcc
