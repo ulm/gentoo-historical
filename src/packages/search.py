d3d954f32bf9018c404678b044b39ee015aef349
Albert Hopkins <marduk@gentoo.org>
2007-01-06 01:04:09 +0000

    Moved "similar" functionality to it's own module.  Removed unused "col" variable.  Changed search from regex-based to keyword based.

--- src/packages/search.py
+++ src/packages/search.py
@@ -13,13 +13,16 @@ type = form.getvalue("type","")
 sstring = form.getvalue("sstring","")
 offset = form.getvalue("offset","0")
 
+# we score search results
+POINTS = {
+	'exact_match': 25,
+	'starts_with': 20,
+	'in_name': 10,
+	'in_description': 2
+}
+
 if not sstring:
 	sys.exit(0)
-
-if type == 'desc':
-	col = "description"
-else:
-	col = "name"
 
 def query_to_dict(q):
 	pkginfo = {}
@@ -39,17 +42,44 @@ def write_to_cache(s):
 
 def sort_by_weight(a, b):
     """Right now we just sort based on whether the sstring is in the name"""
-    a_name = a['name']
-    b_name = b['name']
     
-    a_match = re.search(sstring, a_name, re.IGNORECASE)
-    b_match = re.search(sstring, b_name, re.IGNORECASE)
-    
-    if a_match and b_match:
-        return 0
-    if a_match:
-        return -1
-    return 1
+    for i in [a, b]:
+        if i.has_key('score'):
+	        # We've already scored this one.  Skip it
+            continue
+
+        i['score'] = 0
+        words = [word.lower() for word in sstring.split()]
+        desc = i['description'].lower()
+
+        wlen = len(words)
+        x = wlen + 1
+
+        for word in words:
+            name = i['name'].lower()
+            if name == word:
+                i['score'] = i['score'] + POINTS['exact_match']
+            if name.startswith(sstring):
+                i['score'] = i['score'] + POINTS['starts_with']
+
+            if word in name:
+                i['score'] = i['score'] + POINTS['in_name']
+            i['score'] = i['score'] + (desc.count(word) 
+                    * POINTS['in_description']) * x
+            x = x -1
+        
+        all_words = True
+        desc_words = desc.split()
+        for word in words:
+            if word not in desc_words:
+                all_words = False
+                break
+        if all_words:
+            i['score'] = i['score'] * 2
+                
+    return cmp(b['score'], a['score'])
+
+
 
 # if it's in the cache, just write that out
 qs = sys.argv[1]
@@ -59,15 +89,24 @@ if os.path.exists(cachefile):
 	sys.stdout.write(open(cachefile,'r').read())
 	sys.exit(0)
 
-escaped = escape_string(sstring)
+words = sstring.split()
+criteria = ''
+for word in words:
+    esc_word = escape_string(word)
+    criteria += ('OR name REGEXP "[[:<:]]%s" '
+            'OR description REGEXP "[[:<:]]%s[[:>:]]" ' % (esc_word, esc_word))
 query = ('SELECT category,name,homepage,description,license '
-	'FROM package WHERE name REGEXP "%s" or description REGEXP "%s" '
-	'ORDER BY category LIMIT %s,%s' 
-	% (escaped,escaped,offset,config.MAXPERPAGE))
+	'FROM package  WHERE 1=0 %s' % criteria)
 
 import ebuilddb
 db = ebuilddb.db_connect()
 c = db.cursor()
+c.execute(query)
+total_rows = c.rowcount
+
+# now run the query again with LIMITS
+#query = '%s LIMIT %s,%s' % (query, offset, config.MAXPERPAGE)
+
 try:
 	c.execute(query)
 	results = c.fetchall()
@@ -92,7 +131,12 @@ if not results:
 pkgs = [ query_to_dict(i) for i in results ]
 pkgs.sort(sort_by_weight)
 
-s = '\n'.join([gentoo.package_to_html(pkg,db) for pkg in pkgs])
+pkgs = pkgs[int(offset): int(offset) + config.MAXPERPAGE]
+lresults = len(pkgs)
+
+s = '<b>Results %s - %s of %s</b><br>' % (int(offset) + 1, 
+	int(offset) + lresults, total_rows) 
+s = '\n'.join([s] + [gentoo.package_to_html(pkg,db) for pkg in pkgs])
 #for pkg in pkgs:
 #	#print pkg
 #	html = gentoo.package_to_html(pkg,db)
@@ -100,7 +144,7 @@ s = '\n'.join([gentoo.package_to_html(pkg,db) for pkg in pkgs])
 
 if offset != "0":
 	s = '%s<a href="?sstring=%s;offset=%s">[Previous]</a> ' % (s,sstring,int(offset) - config.MAXPERPAGE)
-if len(results) == config.MAXPERPAGE:
+if int(offset) + lresults < total_rows:
 	s = '%s<a href="?sstring=%s;offset=%s">[Next]</a> ' % (s,sstring,int(offset) + config.MAXPERPAGE)
 
 sys.stdout.write(s)
