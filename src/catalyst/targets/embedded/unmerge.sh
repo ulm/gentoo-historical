d23ba993b39978717f05c1808a09771ce04c435f
Chris Gianelloni <wolf31o2@gentoo.org>
2007-02-08 19:35:47 +0000

    Removing catalyst and adding a README pointing to the new location.  This should clear up any confusion people are having.

--- src/catalyst/targets/embedded/unmerge.sh
+++
@@ -1,5 +0,0 @@
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/embedded/Attic/unmerge.sh,v 1.4 2006/10/02 20:41:54 wolf31o2 Exp $
-
-${clst_CHROOT} ${clst_chroot_path} /bin/bash << EOF
-	ROOT=/tmp/mergeroot emerge -C $* || exit 1
-EOF
