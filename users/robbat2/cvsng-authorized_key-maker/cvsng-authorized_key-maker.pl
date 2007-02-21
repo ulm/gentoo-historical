4df06e7470e0555bc84dfc4c35797d99d0e79439
Robin H. Johnson <robbat2@gentoo.org>
2007-02-21 08:31:21 +0000

    Always generate authorized_keys for every user, even those that are special. Instead dont apply the restrictions for the special users.

--- users/robbat2/cvsng-authorized_key-maker/cvsng-authorized_key-maker.pl
+++ users/robbat2/cvsng-authorized_key-maker/cvsng-authorized_key-maker.pl
@@ -3,7 +3,7 @@
 ### LDAP->authorized_keys creation tool
 ### Copyright 2007 Robin H. Johnson <robbat2@gentoo.org>
 ###
-### $Header: /var/cvsroot/gentoo/users/robbat2/cvsng-authorized_key-maker/cvsng-authorized_key-maker.pl,v 1.2 2007/02/17 14:30:21 robbat2 Exp $
+### $Header: /var/cvsroot/gentoo/users/robbat2/cvsng-authorized_key-maker/cvsng-authorized_key-maker.pl,v 1.3 2007/02/21 08:31:21 robbat2 Exp $
 ###
 ### This program is free software; you can redistribute it and/or modify
 ### it under the terms of the GNU General Public License as published by
@@ -15,6 +15,7 @@ use warnings;
 use Encode;
 use Net::LDAP;
 use Time::Local;
+use Time::HiRes qw(gettimeofday);
 use Getopt::Std;
 use Unicode::Normalize;
 use POSIX qw(strftime);
@@ -60,7 +61,6 @@ foreach (@msg) {
 	if(grep {/$directusersregex/} @$gentooAccess) {
 		print "Skipping\n";
 		$specialaccess = 1;
-		next;
 	}
 	my @raw_keys_ssh2 = grep (/ssh-/,@$sshPublicKey);
 	# Clean up keys that had embedded newlines
@@ -84,9 +84,16 @@ foreach (@msg) {
 	mkdir $ssh_dir,0700;
 	chown $uidNumber,0, $home_dir, $ssh_dir;
 	open(AKEYS,'>',$ssh_dir.'/authorized_keys.tmp');
+	my ($s, $ms) = gettimeofday();
+	print AKEYS '# '.$ssh_dir.'/authorized_keys created '.strftime("%a, %d %b %Y %H:%M:%S.".sprintf("%06d",$ms)." %z",gmtime($s))."\n";
 	foreach(@keys_ssh2) {
 		print "writing-key ";
-		print AKEYS $commandstring." ".$_."\n";
+		my $prefix='';
+		# If they aren't special, rip out prefix of key
+		$_ =~ s/^.*ssh-/ssh-/g if ($specialaccess != 1);
+		# and force it to what we want it to be.
+		$prefix = $commandstring.' ' if ($specialaccess != 1);
+		print AKEYS $prefix.$_."\n";
 	}
 	close AKEYS;
 	chown $uidNumber,-1, $authorized_keys, $authorized_keys_temp;
