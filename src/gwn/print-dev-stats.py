8db70dda514987863a29bda70ce14f15f6b8b051
Anant Narayanan <anant@gentoo.org>
2008-05-21 00:41:07 +0000

    Add dev stats script

---
+++ src/gwn/print-dev-stats.py
@@ -0,0 +1,17 @@
+import xml.dom.minidom
+
+dom = xml.dom.minidom.parse('/home/anant/gentoo/gentoo/xml/htdocs/proj/en/devrel/roll-call/userinfo.xml')
+aom = xml.dom.minidom.parse('devaway.xml')
+
+dev = dom.getElementsByTagName('user')
+tot = len(dev)
+awa = aom.getElementsByTagName('dev')
+awa = len(awa)
+cnt = 0
+
+for d in dev:
+	ret = d.getElementsByTagName('status')
+	if not ret:
+		cnt += 1
+
+print tot, cnt, awa
