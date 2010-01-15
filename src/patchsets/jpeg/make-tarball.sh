256a61ba900675991b7b81878dc1ba67293fd8cb
Mike Frysinger <vapier@gentoo.org>
2010-01-15 05:02:06 +0000

    support patchsets of versioned subdirs

--- src/patchsets/jpeg/make-tarball.sh
+++ src/patchsets/jpeg/make-tarball.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]] ; then
-	echo "Usage: $0 <patch ver>"
+ver=$1
+pver=$2
+
+if [[ $# -ne 2 ]] ; then
+	echo "Usage: $0 <pkg ver> <patch ver>"
 	exit 1
 fi
-pver=$1
 
-tar=jpeg-6b-patches-${pver}.tar.bz2
+tar=jpeg-${ver}-patches-${pver}.tar.bz2
 
 rm -rf tmp
 rm -f ${tar}
 
 mkdir -p tmp/patch
-cp patches/*.patch tmp/patch/ || exit 1
-mkdir -p tmp/extra
-cp `find extra -type f '!' -path '*/CVS*/'` tmp/extra/ || exit 1
+cp ${ver}/*.patch tmp/patch/ || exit 1
 cp ../README* tmp/
 
 tar -jcf ${tar} -C tmp . || exit 1
