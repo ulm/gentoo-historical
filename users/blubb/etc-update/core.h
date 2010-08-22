b2145ceeb7b0f10e826dc259508924d8228f523f
Mike Frysinger <vapier@gentoo.org>
2010-08-22 21:12:43 +0000

    dead

--- users/blubb/etc-update/core.h
+++
@@ -1,6 +0,0 @@
-char *get_real_filename(const char *update);
-char *get_highest_update(char **index, char *update);
-bool is_last_entry(const char *entry);
-bool is_valid_entry(const char *entry);
-void merge(char *update, char **index);
-void show_diff(char *update);
--- users/blubb/etc-update/index.h
+++
@@ -1,3 +0,0 @@
-char **find_updates(char *searchdir);
-MENU *create_menu(char **protected);
-void remove_menu(MENU *mymenu);
