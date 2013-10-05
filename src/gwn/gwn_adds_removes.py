8c285e25d74a1b20176645abafed37c053638839
Markos Chandras <hwoarang@gentoo.org>
2013-10-05 15:15:41 +0000

    gwn_adds_removes.py: Update and clean up
    Move away from GuideXML specific code and remove noise.
    Fix a couple of prolems with parsing the list of developers and drop
    the --attic option. We will always use hyperlinks even for dead packages

--- src/gwn/gwn_adds_removes.py
+++ src/gwn/gwn_adds_removes.py
@@ -8,9 +8,9 @@ ATTIC_LINK = 'http://sources.gentoo.org/viewcvs.py/gentoo-x86/%s/%s/?hideattic=0
 OUTPUT_CHARSET = 'utf-8'
 
 def parse(logfile):
-	
+
 	# Read the log file
-	
+
 	pkgs = {'added': [], 'removed': []}
 	log = open(logfile)
 	for i in log:
@@ -24,9 +24,9 @@ def parse(logfile):
 		else:
 			cur.append(i)
 	log.close()
-	
+
 	# Parse the information
-	
+
 	for op in ('added', 'removed'):
 		for idx, pkg in enumerate(pkgs[op]):
 			pkg = pkg.split(',')
@@ -34,74 +34,46 @@ def parse(logfile):
 			dev = pkg[2]
 			dt = datetime(*(time.strptime(pkg[3], '%Y-%m-%d %H:%M:%S')[0:6]))
 			pkgs[op][idx] = (cp, dev, dt)
-	
+
 	return pkgs
 
 def developers():
-	
+
 	html = urllib2.urlopen(DEV_LIST).read()
 	doc = libxml2.htmlParseDoc(html, 'utf-8')
 	rows = doc.xpathEval('//table[@class = "ntable"]/tr')
 	devs = {}
-	
+
 	for i in rows[1:]:
-		ths = i.xpathEval('th/b')
 		tds = i.xpathEval('td')
-		devs[ths[0].content] = tds[0].content.decode('utf-8')
-	
+		devs[tds[0].content] = tds[0].content.decode('utf-8')
+
 	doc.freeDoc()
 	return devs
-	
 
-def write(data, devs, attic=False):
-	
+
+def write(data, devs):
+
 	sections = {'added': 'Addition', 'removed': 'Removal'}
 	for s in sections:
-		
-		print '<section>'
-		print '<title>%s:</title>' % (sections[s] + 's')
-		print '<body>'
-		print ''
-		
-		print '<table>'
-		print ''
-		print '<tr>'
-		print '<th>Package:</th>'
-		print '<th>%s date:</th>' % sections[s]
-		print '<th>Contact:</th>'
-		print '</tr>'
-		print ''
-		
 		for pkg in data[s]:
-			print '<tr>'
 			ldata = (PACKAGE_LINK % pkg[0], '/'.join(pkg[0]))
 			if s == 'added':
-				print '<ti><uri link="%s">%s</uri></ti>' % ldata
+				what = "added"
 			else:
-				if attic:
-					ldata = (ATTIC_LINK % pkg[0], '/'.join(pkg[0]))
-					print '<ti><uri link="%s">%s</uri></ti>' % ldata
-				else:
-					print '<ti>%s</ti>' % ldata[1]
-			print '<ti>%s</ti>' % pkg[2].strftime('%d %b %Y')
-			mdata = (pkg[1], devs[pkg[1]].encode(OUTPUT_CHARSET))
-			print '<ti><mail link="%s@gentoo.org">%s</mail></ti>' % mdata
-			print '</tr>'
-			print ''
-		
-		print '</table>'
-		print ''
-		
-		print '</body>'
-		print '</section>'
-		print ''
-
+				what = "removed"
+				ldata = (ATTIC_LINK % pkg[0], '/'.join(pkg[0]))
+			#who = (devs[pkg[1]].encode(OUTPUT_CHARSET), pkg[1])
+			who = devs[pkg[1]]
+			when = pkg[2].strftime('%d %b %Y')
+			# You should copy this to the raw html code in the
+			# blog post
+			print '<a href="%s">%s</a> %s by %s on %s' % (ldata[0], ldata[1], what, who,when)
 
 if __name__ == '__main__':
-	if len(sys.argv) < 2 or len(sys.argv) > 3:
-		print 'Usage: gwn_adds_removes.py <log-file> [--attic]'
+	if len(sys.argv) != 2:
+		print 'Usage: gwn_adds_removes.py <log-file>'
 	else:
 		data = parse(sys.argv[1])
 		devs = developers()
-		attic = len(sys.argv) == 3 and sys.argv[2] == '--attic'
-		write(data, devs, attic)
+		write(data, devs)
