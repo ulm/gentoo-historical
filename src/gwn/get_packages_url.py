11b136f1426b9aa11d37213f0f925a460c2e7904
Markos Chandras <hwoarang@gentoo.org>
2013-10-13 09:09:02 +0000

    get_packages_url: Use it to gather all portage statistics

--- src/gwn/get_packages_url.py
+++ src/gwn/get_packages_url.py
@@ -19,24 +19,58 @@ arch = [
 'x86',
 'x86-fbsd']
 
+gen_stats = [
+'architectures',
+'categories',
+'packages',
+'ebuilds']
+
+gen_stats_num = []
+
 stable = []
 testing = []
+total = []
+percent = []
 
 svals = []
 tvals = []
 tot = []
 
-
-os.system("qcache -sCq | tail -n 47 | head -n 16 | cut -d \"|\" -f 2-4 | sed \"s:|::g\" > pkgstats.txt")
+# Get general portage stats
+os.system("qcache  -sCq | sed \"s:|::g\" > pkgstats.txt")
 
 # read input files
 with open("pkgstats.txt") as f:
 	for line in f:
 		line=line.split()
-		stable.append(int(line[1]))
-		testing.append(int(line[2]))
+		try:
+			col = line[0].strip()
+		except:
+			continue
+		if col in gen_stats:
+			gen_stats_num.append(str(line[1].strip()))
+		elif col in arch:
+			#print line[0] + ", " + line[1] + ", " + line[2] +", " + \
+			#	line[3] + ", " + line[4]
+			stable.append(int(line[1]))
+			testing.append(int(line[2]))
+			total.append(int(line[3]))
+			percent.append(line[4])
 f.close()
 os.remove("pkgstats.txt")
+
+print "[table]"
+print "Architectures, Categories, Packages, Ebuilds"
+print gen_stats_num[0] + ", " + gen_stats_num[1] + ", " + \
+	gen_stats_num[2] + ", " + gen_stats_num[3]
+print "[/table]\n"
+
+print "[table]"
+print "Architecture, Stable, Testing, Total, % of Packages"
+for x in range(0, len(arch)):
+	print arch[x] + ", " + str(stable[x]) + ", " + \
+	str(testing[x]) + ", " + str(total[x]) + ", " + percent[x]
+print "[/table]\n"
 
 for i in range(0, len(arch)):
 	svals.append(round(stable[i] / 123.81, 2))
@@ -53,6 +87,7 @@ for i in range(0, len(svals)):
 		string2 += ','
 #print string1
 #print string2
+print "\n== Chart URL == \n"
 base = 'http://chart.apis.google.com/chart?cht=bhs&chs=800x350&chd=t:'
 tail = '&chco=45347B,6A5C95&chbh=15&chxt=x,y&chxr=0,0,12250&chdl=Stable|Testing'
 labx = '&chxl=0:1000|1000|1000|1000|1000|1000|1000|1000|1000|1000|1000|1000|1000|1000|1000|'
