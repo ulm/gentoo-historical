09d31f6eeb0241a51f929365e23212b5fa426f50
Markos Chandras <hwoarang@gentoo.org>
2013-10-13 10:32:43 +0000

    gmn_bugzie_aggregate.py: Make it work with new gmn_bugzie.py
    The gmn_bugzie.py prints output suitable for the GMN Blog entries but this
    broke compatibility with the "_aggregate.py" one. This is now fixed, and the
    obsolete GMN GuideXML text has also been removed

--- src/gwn/gmn_bugzie_aggregate.py
+++ src/gwn/gmn_bugzie_aggregate.py
@@ -13,15 +13,34 @@ opkeys = []
 opkraw = []
 opdata = []
 
-j = 0
+j = 1
+ontable = 0
+onheaders = 0
+ontext = 0
 for i in data:
-	values = i.strip('\n').split(':')
-	# j == 0 -> New, Closed, Not Fixed, Duplicates, Total
-	if j == 0:
-		# Dates
-		dfrom = values[0]
-		dnext = values[1]
-	elif j > 0 and j <= 4:
+	if not i.startswith("[table]") and ontable == 0:
+		continue
+	ontable = 1
+	# Ok found a table, we need to skip the
+	# [table] and headers lines
+	if onheaders == 0: # [table]
+		onheaders = 1
+		continue
+	if onheaders == 1 and ontext == 0: # [headers]
+		ontext = 1
+		continue
+	# reached the end of the table, need to start over
+	if i.startswith("[/table]"):
+		ontext = 0
+		onheaders = 0
+		ontable = 0
+		continue
+	#Here is the real text
+	values = i.strip().split(',')
+	# A few tables contain ranking numbers. Remove them
+	if len(values) == 3:
+		values.pop(0)
+	if j > 0 and j <= 4:
 		# Populate list of New, Closed, Not Fixed and Duplicates Bugs
 		budata.append(int(values[1]))
 		bukeys.append(values[0] + ' (' + str(budata[-1]) + ')')
@@ -76,100 +95,5 @@ opened.download('opened.png')
 
 clkeys.reverse()
 opkeys.reverse()
-print """<chapter>
-<title>Bugzilla</title>
 
-<section>
-<title>Statistics</title>
-<body>
-
-<p>
-The Gentoo community uses Bugzilla
-(<uri link="http://bugs.gentoo.org">bugs.gentoo.org</uri>) to record
-and track bugs, notifications, suggestions and other interactions
-with the development team. The following chart summarizes activity on
-Bugzilla between %s and %s.
-</p>
-
-<figure link="activity.png" short="Bug activity" caption="Bug activity split-up"/>
-
-<p>
-Of the <b>%d</b> currently open bugs: <b>%d</b> are labeled <e>blocker</e>,
-<b>%d</b> are labeled <e>critical</e>, and <b>%d</b> are labeled <e>major</e>.
-</p>
-
-</body>
-</section>
-""" % (dfrom, dnext, butota[0], butota[1], butota[2], butota[3])
-
-print """<section>
-<title>Closed bug ranking</title>
-<body>
-
-<p>
-The developers and teams who have closed the most bugs during this period are as follows.
-</p>
-
-<table>
-<tr>
-  <th>Rank</th>
-  <th>Developer/Team</th>
-  <th>Bug Count</th>
-</tr>
-"""
-print """<tr>
-  <ti>0</ti>
-  <ti>Others</ti>
-  <ti>%d</ti>
-</tr>""" % clothers
-
-for i in range(0, len(cldata)):
-	print """<tr>
-  <ti>%d</ti>
-  <ti>%s</ti>
-  <ti>%d</ti>
-</tr>""" % (i+1, clkraw[i], cldata[i])
-
-print """</table>
-
-<figure link="closed.png" short="Bugs closed" caption="Bug closed rankings"/>
-
-</body>
-</section>
-
-<section>
-<title>Assigned bug ranking</title>
-<body>
-
-<p>
-The developers and teams who have been assigned the most bugs during this period are as follows.
-</p>
-
-<table>
-<tr>
-  <th>Rank</th>
-  <th>Developer/Team</th>
-  <th>Bug Count</th>
-</tr>
-"""
-print """<tr>
-  <ti>0</ti>
-  <ti>Others</ti>
-  <ti>%d</ti>
-</tr>""" % opothers
-
-for i in range(0, len(opdata)):
-	print """<tr>
-  <ti>%d</ti>
-  <ti>%s</ti>
-  <ti>%d</ti>
-</tr>""" % (i+1, opkraw[i], opdata[i])
-
-print """</table>
-
-<figure link="opened.png" short="Bugs assigned" caption="Bugs assigned rankings"/>
-
-</body>
-</section>
-</chapter>
-"""
+print "Done!"
